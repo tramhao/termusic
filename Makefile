c4f9f295b4505be987d594a37aadc9f9f739250c
tramhao <haoxinkui@gmail.com>
2021-06-02 11:48:35 +0800

    app.rs separated

--- Makefile
+++ Makefile
@@ -1,14 +1,16 @@
 prog := music_tui_rs
 
+default: fmt debug
+
 fmt:
 	cargo fmt
 
-default: fmt
+debug: 
 	cargo build
-
 
 release:
 	cargo build --release
+
 
 install: fmt release
 	cp target/release/$(prog) ~/.local/share/cargo/bin/
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod app;
+
+use crate::app::App;
 use anyhow::Result;
 use crossterm::{
     event::{self, Event as CEvent, KeyCode},
@@ -11,11 +14,10 @@ use std::sync::mpsc;
 use std::thread;
 use std::time::{Duration, Instant};
 use thiserror::Error;
-use tui::backend::CrosstermBackend;
-use tui::layout::{Alignment, Constraint, Direction, Layout};
-use tui::style::{Color, Style};
-use tui::widgets::{Block, BorderType, Borders, Paragraph};
-use tui::Terminal;
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    Terminal,
+};
 
 #[derive(Error, Debug)]
 pub enum Error {
@@ -33,6 +35,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     defer! {
         shutdown_terminal();
     }
+    let app = App::new();
 
     // enable_raw_mode().expect("can run in raw mode");
 
@@ -62,55 +65,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut terminal = start_terminal(io::stdout())?;
 
     loop {
-        terminal.draw(|rect| {
-            let size = rect.size();
-            let chunks = Layout::default()
-                .direction(Direction::Horizontal)
-                .margin(0)
-                .constraints([Constraint::Ratio(3, 10), Constraint::Ratio(7, 10)].as_ref())
-                .split(size);
-            let chunks2 = Layout::default()
-                .direction(Direction::Vertical)
-                .margin(0)
-                .constraints([Constraint::Min(2), Constraint::Length(9)].as_ref())
-                .split(chunks[1]);
-
-            let copyright = Paragraph::new("pet-CLI 2020 - all rights reserved")
-                .style(Style::default().fg(Color::LightCyan))
-                .alignment(Alignment::Center)
-                .block(
-                    Block::default()
-                        .borders(Borders::ALL)
-                        .style(Style::default().fg(Color::White))
-                        .title("Copyright")
-                        .border_type(BorderType::Plain),
-                );
-            let copyright2 = Paragraph::new("pet-CLI 2020 - all rights reserved")
-                .style(Style::default().fg(Color::LightCyan))
-                .alignment(Alignment::Center)
-                .block(
-                    Block::default()
-                        .borders(Borders::ALL)
-                        .style(Style::default().fg(Color::White))
-                        .title("Copyright")
-                        .border_type(BorderType::Plain),
-                );
-            let copyright3 = Paragraph::new("pet-CLI 2020 - all rights reserved")
-                .style(Style::default().fg(Color::LightCyan))
-                .alignment(Alignment::Center)
-                .block(
-                    Block::default()
-                        .borders(Borders::ALL)
-                        .style(Style::default().fg(Color::White))
-                        .title("Copyright")
-                        .border_type(BorderType::Plain),
-                );
-
-            rect.render_widget(copyright, chunks[0]);
-            rect.render_widget(copyright2, chunks2[0]);
-            rect.render_widget(copyright3, chunks2[1]);
-        })?;
-
+        draw(&mut terminal, &app)?;
         match rx.recv()? {
             Event::Input(event) => match event.code {
                 KeyCode::Char('Q') => {
@@ -118,7 +73,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     terminal.show_cursor()?;
                     break;
                 }
-                // KeyCode::Char('p') => active_menu_item = MenuItem::Pets,
                 _ => {}
             },
             Event::Tick => {}
@@ -155,4 +109,18 @@ fn start_terminal<W: Write>(buf: W) -> io::Result<Terminal<CrosstermBackend<W>>>
     terminal.clear()?;
 
     Ok(terminal)
+}
+
+fn draw<B: Backend>(terminal: &mut Terminal<B>, app: &App) -> io::Result<()> {
+    if app.requires_redraw() {
+        terminal.resize(terminal.size()?)?;
+    }
+
+    terminal.draw(|mut f| {
+        if let Err(e) = app.draw(&mut f) {
+            log::error!("failed to draw: {:?}", e)
+        }
+    })?;
+
+    Ok(())
 }
