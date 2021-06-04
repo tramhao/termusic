6796f6d35a1b178f07d4f82f57ee462782e843c1
tramhao <haoxinkui@gmail.com>
2021-06-05 00:21:38 +0800

    move everything into app.rs

--- src/app.rs
+++ src/app.rs
@@ -1,29 +1,96 @@
 use super::utils::keymap::MSG_KEY_ESC;
 
+use super::utils::myinput::InputHandler;
+use super::utils::terminal::Terminal;
 use std::time::Instant;
 
-use tuirealm::components::label;
-use tuirealm::{Msg, Payload, PropsBuilder, Update, Value, View};
+use std::thread::sleep;
+use std::time::Duration;
+
+use tuirealm::components::{input, label};
+use tuirealm::props::borders::{BorderType, Borders};
+use tuirealm::{InputType, PropsBuilder, Update, View};
 // tui
+use tui::layout::{Constraint, Direction, Layout};
+use tui::style::Color;
 
 const COMPONENT_INPUT: &str = "INPUT";
 const COMPONENT_LABEL: &str = "LABEL";
+
+use tuirealm::{Msg, Payload, Value};
+// tui
 
 pub struct App {
     pub quit: bool,           // Becomes true when the user presses <ESC>
     pub redraw: bool,         // Tells whether to refresh the UI; performance optimization
     pub last_redraw: Instant, // Last time the ui has been redrawed
     pub view: View,
+    pub context: Option<Terminal>,
 }
 
 impl App {
-    pub fn new(view: View) -> Self {
+    pub fn new() -> Self {
+        let mut terminal: Terminal = Terminal::new();
+        // Enter alternate screen
+        terminal.enter_alternate_screen();
+        // Clear screen
+        terminal.clear_screen();
+
+        let mut myview: View = View::init();
+        // Let's mount the component we need
+        myview.mount(
+            COMPONENT_INPUT,
+            Box::new(input::Input::new(
+                input::InputPropsBuilder::default()
+                    .with_borders(Borders::ALL, BorderType::Rounded, Color::LightYellow)
+                    .with_foreground(Color::LightYellow)
+                    .with_input(InputType::Text)
+                    .with_label(String::from("Type in something nice"))
+                    .build(),
+            )),
+        );
+        myview.mount(
+            COMPONENT_LABEL,
+            Box::new(label::Label::new(
+                label::LabelPropsBuilder::default()
+                    .with_foreground(Color::Cyan)
+                    .with_text(String::from("Your input will appear in after a submit"))
+                    .build(),
+            )),
+        );
+        // We need to initialize the focus
+        myview.active(COMPONENT_INPUT);
+
         App {
             quit: false,
             redraw: true,
             last_redraw: Instant::now(),
-            view,
+            view: myview,
+            context: Some(terminal),
         }
+    }
+
+    pub fn run(&mut self) {
+        let input: InputHandler = InputHandler::new();
+        while !self.quit {
+            // Listen for input events
+            if let Ok(Some(ev)) = input.read_event() {
+                // Pass event to view
+                let msg = self.view.on(ev);
+                self.redraw();
+                // Call the elm friend update
+                self.update(msg);
+            }
+            // If redraw, draw interface
+            if self.redraw || self.last_redraw.elapsed() > Duration::from_millis(50) {
+                // Call the elm friend vie1 function
+                self.view();
+                self.reset();
+            }
+            sleep(Duration::from_millis(10));
+        }
+
+        drop(self.context.take());
     }
 
     pub fn quit(&mut self) {
@@ -34,11 +101,24 @@ impl App {
         self.redraw = true;
     }
 
+    fn view(&mut self) {
+        let mut ctx: Terminal = self.context.take().unwrap();
+        let _ = ctx.terminal.draw(|f| {
+            // Prepare chunks
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(1)
+                .constraints([Constraint::Length(3), Constraint::Length(3)].as_ref())
+                .split(f.size());
+            self.view.render(COMPONENT_INPUT, f, chunks[0]);
+            self.view.render(COMPONENT_LABEL, f, chunks[1]);
+        });
+        self.context = Some(ctx);
+    }
     pub fn reset(&mut self) {
         self.redraw = false;
         self.last_redraw = Instant::now();
     }
-
 }
 
 impl Update for App {
--- src/main.rs
+++ src/main.rs
@@ -2,87 +2,8 @@ mod app;
 mod utils;
 
 use app::App;
-use utils::myinput::InputHandler;
-use utils::terminal::Terminal;
-
-use std::thread::sleep;
-use std::time::Duration;
-
-use tuirealm::components::{input, label};
-use tuirealm::props::borders::{BorderType, Borders};
-use tuirealm::{InputType, PropsBuilder, Update, View};
-// tui
-use tui::layout::{Constraint, Direction, Layout};
-use tui::style::Color;
-
-const COMPONENT_INPUT: &str = "INPUT";
-const COMPONENT_LABEL: &str = "LABEL";
 
 fn main() {
-    let mut terminal: Terminal = Terminal::new();
-    let input: InputHandler = InputHandler::new();
-    // Enter alternate screen
-    terminal.enter_alternate_screen();
-    // Clear screen
-    terminal.clear_screen();
-
-    let mut myview: View = View::init();
-    // Let's mount the component we need
-    myview.mount(
-        COMPONENT_INPUT,
-        Box::new(input::Input::new(
-            input::InputPropsBuilder::default()
-                .with_borders(Borders::ALL, BorderType::Rounded, Color::LightYellow)
-                .with_foreground(Color::LightYellow)
-                .with_input(InputType::Text)
-                .with_label(String::from("Type in something nice"))
-                .build(),
-        )),
-    );
-    myview.mount(
-        COMPONENT_LABEL,
-        Box::new(label::Label::new(
-            label::LabelPropsBuilder::default()
-                .with_foreground(Color::Cyan)
-                .with_text(String::from("Your input will appear in after a submit"))
-                .build(),
-        )),
-    );
-    // We need to initialize the focus
-    myview.active(COMPONENT_INPUT);
-
-    let mut model: App = App::new(myview);
-
-    while !model.quit {
-        // Listen for input events
-        if let Ok(Some(ev)) = input.read_event() {
-            // Pass event to view
-            let msg = model.view.on(ev);
-            model.redraw();
-            // Call the elm friend update
-            model.update(msg);
-        }
-        // If redraw, draw interface
-        if model.redraw || model.last_redraw.elapsed() > Duration::from_millis(50) {
-            // Call the elm friend vie1 function
-            view(&mut terminal, &mut model.view);
-            model.reset();
-        }
-        sleep(Duration::from_millis(10));
-    }
-
-    drop(terminal);
-}
-
-fn view(t: &mut Terminal, view: &View) {
-    let _ = t.terminal.draw(|f| {
-        // Prepare chunks
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .margin(1)
-            .constraints([Constraint::Length(3), Constraint::Length(3)].as_ref())
-            .split(f.size());
-        view.render(COMPONENT_INPUT, f, chunks[0]);
-        view.render(COMPONENT_LABEL, f, chunks[1]);
-    });
+    let mut app: App = App::new();
+    app.run();
 }
