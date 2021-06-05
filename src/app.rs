fd0fdc7b53101aab8e94a019a5bf6a758b097743
tramhao <haoxinkui@gmail.com>
2021-06-06 03:00:27 +0800

    finish activity

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,9 @@
+use super::ui::activity::main::MainActivity;
+use super::ui::activity::{Activity, ExitReason};
 use super::ui::context::Context;
-use super::ui::inputhandler::InputHandler;
 use std::time::Instant;
 
+use log::error;
 use std::thread::sleep;
 use std::time::Duration;
 
@@ -31,38 +33,31 @@ impl App {
     }
 
     pub fn run(&mut self) {
-        let input: InputHandler = InputHandler::new();
-        while !self.quit {
-            // Listen for input events
-            if let Ok(Some(ev)) = input.read_event() {
-                // Pass event to view
-                // let msg = self.view.on(ev);
-                self.redraw();
-                // Call the elm friend update
-                // self.update(msg);
+        let mut activity: MainActivity = MainActivity::default();
+        // Get context
+        let ctx: Context = match self.context.take() {
+            Some(ctx) => ctx,
+            None => {
+                error!("Failed to start SetupActivity: context is None");
+                return;
             }
-            // If redraw, draw interface
-            if self.redraw || self.last_redraw.elapsed() > Duration::from_millis(50) {
-                // Call the elm friend vie1 function
-                // self.view();
-                self.reset();
+        };
+        // Create activity
+        activity.on_create(ctx);
+        loop {
+            // Draw activity
+            activity.on_draw();
+            // Check if activity has terminated
+            if let Some(ExitReason::Quit) = activity.will_umount() {
+                // info!("SetupActivity terminated due to 'Quit'");
+                break;
             }
+            // Sleep for ticks
             sleep(Duration::from_millis(10));
         }
+        // Destroy activity
+        self.context = activity.on_destroy();
 
         drop(self.context.take());
-    }
-
-    fn quit(&mut self) {
-        self.quit = true;
-    }
-
-    fn redraw(&mut self) {
-        self.redraw = true;
-    }
-
-    fn reset(&mut self) {
-        self.redraw = false;
-        self.last_redraw = Instant::now();
     }
 }
