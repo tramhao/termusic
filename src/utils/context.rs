5940e7f6d1845ab0c4800619d1799f84c9c276d8
tramhao <haoxinkui@gmail.com>
2021-06-06 00:20:12 +0800

    forget to add new file to git

---
+++ src/utils/context.rs
@@ -0,0 +1,55 @@
+extern crate crossterm;
+extern crate tui;
+
+use crossterm::event::DisableMouseCapture;
+use crossterm::execute;
+use crossterm::terminal::{
+    disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
+};
+use std::io::{stdout, Stdout};
+use tui::backend::CrosstermBackend;
+use tui::Terminal as TuiTerminal;
+
+pub struct Context {
+    pub context: TuiTerminal<CrosstermBackend<Stdout>>,
+}
+
+impl Context {
+    pub fn new() -> Self {
+        let _ = enable_raw_mode();
+        // Create terminal
+        let mut stdout = stdout();
+        assert!(execute!(stdout, EnterAlternateScreen).is_ok());
+        Self {
+            context: TuiTerminal::new(CrosstermBackend::new(stdout)).unwrap(),
+        }
+    }
+
+    pub fn enter_alternate_screen(&mut self) {
+        let _ = execute!(
+            self.context.backend_mut(),
+            EnterAlternateScreen,
+            DisableMouseCapture
+        );
+    }
+
+    pub fn leave_alternate_screen(&mut self) {
+        let _ = execute!(
+            self.context.backend_mut(),
+            LeaveAlternateScreen,
+            DisableMouseCapture
+        );
+    }
+
+    pub fn clear_screen(&mut self) {
+        let _ = self.context.clear();
+    }
+}
+
+impl Drop for Context {
+    fn drop(&mut self) {
+        // Re-enable terminal stuff
+        self.leave_alternate_screen();
+        let _ = disable_raw_mode();
+    }
+}
