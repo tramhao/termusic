c94f3957721bfb287e11c2869040de18b26cfcdb
tramhao <haoxinkui@gmail.com>
2021-06-15 17:22:58 +0800

    id3 work only for some files, reasons don't know yet

--- src/song.rs
+++ src/song.rs
@@ -1,12 +1,24 @@
 use anyhow::Result;
 use humantime::format_duration;
+use id3::Tag;
 use std::fmt;
+use std::path::Path;
 use std::time::Duration;
 
 #[derive(Clone)]
 pub struct Song {
+    /// Artist of the song
+    pub artist: Option<String>,
+    /// Album of the song
+    pub album: Option<String>,
+    /// Title of the song
+    pub title: Option<String>,
+    /// File path to the song
     pub file: String,
+    /// Duration of the song
     pub duration: Duration,
+    /// name of the song
+    pub name: String,
 }
 
 impl Song {
@@ -16,7 +28,51 @@ impl Song {
             Err(_) => Duration::from_secs(0),
         };
 
-        Ok(Self { file, duration })
+        // let id3_tag = if let Ok(tag) = ::id3::Tag::read_from_path(&file) {
+        //     tag
+        // } else {
+        //     Tag::new()
+        // };
+
+        let id3_tag = Tag::read_from_path(&file).unwrap_or_default();
+        // let artist: Option<String> = Some(String::from(id3_tag.artist().unwrap_or_default()));
+        let artist: Option<String> = id3_tag.artist().and_then(|s| Some(String::from(s)));
+        let album: Option<String> = id3_tag.album().and_then(|s| Some(String::from(s)));
+        let title: Option<String> = id3_tag.title().and_then(|s| Some(String::from(s)));
+        let p: &Path = Path::new(file.as_str());
+        let name = String::from(p.file_name().unwrap().to_string_lossy());
+        Ok(Self {
+            artist,
+            album,
+            title,
+            file,
+            duration,
+            name,
+        })
+    }
+    /// Optionally return the artist of the song
+    /// If `None` it wasn't able to read the tags
+    pub fn artist(&self) -> Option<&str> {
+        match self.artist.as_ref() {
+            Some(artist) => Some(artist),
+            None => None,
+        }
+    }
+    /// Optionally return the song's album
+    /// If `None` failed to read the tags
+    pub fn album(&self) -> Option<&str> {
+        match self.album.as_ref() {
+            Some(album) => Some(album),
+            None => None,
+        }
+    }
+    /// Optionally return the title of the song
+    /// If `None` it wasn't able to read the tags
+    pub fn title(&self) -> Option<&str> {
+        match self.title.as_ref() {
+            Some(title) => Some(title),
+            None => None,
+        }
     }
 }
 
@@ -26,12 +82,12 @@ impl fmt::Display for Song {
         let duration = format_duration(Duration::from_secs(self.duration.as_secs()));
         write!(
             f,
-            "{} - {}",
-            // "{} - {} - {} ({})",
-            // self.artist().unwrap_or("Unknown Artist"),
-            // self.album().unwrap_or("Unknown Album"),
-            // self.title().unwrap_or("Unknown Title"),
-            self.file,
+            // "{} - {}",
+            "{} - {} - {} ({})",
+            self.artist().unwrap_or(self.name.as_ref()),
+            self.title().unwrap_or("Unknown Title"),
+            self.album().unwrap_or("Unknown Album"),
+            // self.file,
             duration,
         )
     }
