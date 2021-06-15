c6ff64a4456e4f37368cedaa736f9f901df29cee
tramhao <haoxinkui@gmail.com>
2021-06-15 15:39:16 +0800

    asdf

--- src/song.rs
+++ src/song.rs
@@ -1,19 +1,35 @@
 use anyhow::Result;
+use humantime::format_duration;
 use std::fmt;
+use std::time::Duration;
 
 #[derive(Clone)]
 pub struct Song {
     pub file: String,
+    pub duration: Duration,
 }
 
 impl Song {
     pub fn load(file: String) -> Result<Self> {
-        Ok(Self { file })
+        let duration = ::mp3_duration::from_path(&file).expect("Error getting duration");
+
+        Ok(Self { file, duration })
     }
 }
 
 impl fmt::Display for Song {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}-{}", self.file, self.file,)
+        // write!(f, "{}-{}", self.file, self.file,)
+        let duration = format_duration(self.duration);
+        write!(
+            f,
+            "{} - {}",
+            // "{} - {} - {} ({})",
+            // self.artist().unwrap_or("Unknown Artist"),
+            // self.album().unwrap_or("Unknown Album"),
+            // self.title().unwrap_or("Unknown Title"),
+            self.file,
+            duration,
+        )
     }
 }
