416bdb148e145a58649a3d888d02a732860971f0
tramhao <haoxinkui@gmail.com>
2021-06-15 15:03:42 +0800

    song struct display trait in progress

--- src/song.rs
+++ src/song.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use std::fmt;
 
 #[derive(Clone)]
 pub struct Song {
@@ -8,5 +9,11 @@ pub struct Song {
 impl Song {
     pub fn load(file: String) -> Result<Self> {
         Ok(Self { file })
+    }
+}
+
+impl fmt::Display for Song {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}-{}", self.file, self.file,)
     }
 }
