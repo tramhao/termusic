c31156e2db8f3a0ca59fc882eb0bd1e6e40d0869
tramhao <haoxinkui@gmail.com>
2021-11-18 01:59:28 +0800

    tick rate

--- src/main.rs
+++ src/main.rs
@@ -160,8 +160,8 @@ fn main() {
     // which we will use to update the clock
     let mut app: Application<Id, Msg, NoUserEvent> = Application::init(
         EventListenerCfg::default()
-            .default_input_listener(Duration::from_millis(50))
-            .poll_timeout(Duration::from_millis(40))
+            .default_input_listener(Duration::from_millis(20))
+            .poll_timeout(Duration::from_millis(400))
             .tick_interval(Duration::from_secs(1)),
     );
 
