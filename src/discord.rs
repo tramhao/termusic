12fa5c9d0691048c6ee7547ec0a4d46f42fbcf1a
tramhao <haoxinkui@gmail.com>
2022-04-27 16:17:50 +0800

    fix panic when no discord running

--- src/discord.rs
+++ src/discord.rs
@@ -6,39 +6,45 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 pub struct Rpc {
     client: DiscordIpcClient,
+    connected: bool,
 }
 
 impl Default for Rpc {
     fn default() -> Self {
         let mut client = DiscordIpcClient::new(APP_ID).unwrap();
-        client.connect().ok();
-        Self { client }
+        let mut connected = false;
+        if let Ok(()) = client.connect() {
+            connected = true;
+        }
+        Self { client, connected }
     }
 }
 
 impl Rpc {
     #[allow(clippy::cast_possible_wrap)]
     pub fn update(&mut self, song: &Song) {
-        let assets = activity::Assets::new()
-            .large_image("termusic")
-            .large_text("terminal music player written in Rust");
-        // .small_image(smol_image)
-        // .small_text(state);
+        if self.connected {
+            let assets = activity::Assets::new()
+                .large_image("termusic")
+                .large_text("terminal music player written in Rust");
+            // .small_image(smol_image)
+            // .small_text(state);
 
-        let time_unix = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .as_secs() as i64;
+            let time_unix = SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_secs() as i64;
 
-        self.client
-            .set_activity(
-                activity::Activity::new()
-                    .assets(assets)
-                    .timestamps(activity::Timestamps::new().start(time_unix))
-                    .state(song.artist().unwrap_or("Unknown Artist"))
-                    .details(song.title().unwrap_or("Unknown Title")),
-            )
-            .ok();
+            self.client
+                .set_activity(
+                    activity::Activity::new()
+                        .assets(assets)
+                        .timestamps(activity::Timestamps::new().start(time_unix))
+                        .state(song.artist().unwrap_or("Unknown Artist"))
+                        .details(song.title().unwrap_or("Unknown Title")),
+                )
+                .ok();
+        }
     }
     // pub fn update_progress(&mut self, duration: u64, progress: u64) {
     //     self.client
@@ -54,6 +60,8 @@ impl Rpc {
 
 impl Drop for Rpc {
     fn drop(&mut self) {
-        self.client.close().ok();
+        if self.connected {
+            self.client.close().ok();
+        }
     }
 }
