49df6d55bd636ec7bda3c2bb3b9f68d8ead9ec82
tramhao <haoxinkui@gmail.com>
2022-05-12 16:38:56 +0800

    in progress

--- src/sqlite.rs
+++ src/sqlite.rs
@@ -28,26 +28,28 @@ impl DataBase {
         // album_photo: Option<String>,
         // file_type: Option<FileType>,
 
-        conn.execute(
-            "create table if not exists directory(
-             id integer primary key
-             ,name text not null
-         )",
-            [],
-        )
-        .expect("creat table directory failed");
+        // conn.execute(
+        //     "create table if not exists directory(
+        //      id integer primary key
+        //      ,name text not null
+        //  )",
+        //     [],
+        // )
+        // .expect("creat table directory failed");
+        // ,directory_id integer not null references directory(id)
+
+        // ,album    TEXT NOT NULL
+        //        ,title    TEXT NOT NULL
+        //        ,file     TEXT UNIQUE
+        //        ,duration DOUBLE
+        //        ,name     TEXT
+        //        ,ext     TEXT
+        //        ,directory_id TEXT
+        //        ,last_modified TEXT
         conn.execute(
             "create table if not exists track(
              id integer primary key
              ,artist   TEXT NOT NULL
-             ,album    TEXT NOT NULL
-             ,title    TEXT NOT NULL
-             ,file     TEXT UNIQUE
-             ,duration DOUBLE
-             ,name     TEXT
-             ,ext     TEXT
-             ,directory_id integer not null references directory(id)
-             ,last_modified TEXT
             )",
             [],
         )
@@ -56,23 +58,27 @@ impl DataBase {
     }
 
     fn add_record(&mut self, track: &Track) -> Result<()> {
+        // self.conn.execute(
+        //     "insert into directory (name) values (?1)",
+        //     &[&track.directory()],
+        // )?;
+        // let last_id: String = self.conn.last_insert_rowid().to_string();
         self.conn.execute(
-            "insert into directory (name) values (?1)",
-            &[&track.directory()],
-        )?;
-        let last_id: String = self.conn.last_insert_rowid().to_string();
-        self.conn.execute(
-            "insert into track (name, file, directory_id,last_modified) values (?1, ?2, ?3, ?4)",
+            // "insert into track (name, file, directory_id,last_modified) values (?1, ?2, ?3, ?4)",
+            // "insert into track (name, file, directory, last_modified) values (?1, ?2, ?3, ?4)",
+            "insert into track (artist) values (?1)",
             &[
-                &track.name().unwrap_or_default().to_string(),
-                &track.file().unwrap_or_default().to_string(),
-                &last_id,
-                &track
-                    .last_modified
-                    .duration_since(UNIX_EPOCH)
-                    .unwrap_or_default()
-                    .as_secs()
-                    .to_string(),
+                "abcdefg",
+                // &track.name().unwrap_or_default().to_string(),
+                // &track.file().unwrap_or_default().to_string(),
+                // // &last_id,
+                // &track.directory().unwrap_or_default().to_string(),
+                // &track
+                //     .last_modified
+                //     .duration_since(UNIX_EPOCH)
+                //     .unwrap_or_default()
+                //     .as_secs()
+                //     .to_string(),
             ],
         )?;
         Ok(())
@@ -81,37 +87,36 @@ impl DataBase {
     pub fn need_update() -> bool {
         todo!()
     }
-
+    // let music_dir = dformat!("{}/**/*.*", MUSIC_DIR);
     pub fn sync_database(&mut self) {
-        // let mut pattern = self.path.clone();
-        // let music_dir = dformat!("{}/**/*.*", MUSIC_DIR);
         let all_items = walkdir::WalkDir::new(self.path.as_path()).follow_links(true);
         for record in all_items.into_iter().filter_map(std::result::Result::ok) {
-            // println!("{}", record.path());
             let track = Track::read_from_path(record.path()).unwrap();
             self.add_record(&track);
         }
-        self.get_record();
+        self.get_record().ok();
     }
     pub fn get_record(&mut self) -> Result<()> {
-        let mut stmt = self.conn.prepare(
-            "SELECT c.name, c.file, c.last_modified, cc.name from track c
-         INNER JOIN directory cc
-         ON cc.id = c.directory;",
-        )?;
+        // let mut stmt = self.conn.prepare(
+        //     "SELECT c.name, c.file, c.last_modified, cc.name from track c
+        //  INNER JOIN directory cc
+        //  ON cc.id = c.directory_id;",
+        // )?;
 
+        let mut stmt = self.conn.prepare("SELECT * FROM track")?;
         let tracks = stmt.query_map([], |row| {
-            let path: String = row.get(1)?;
-            let track = Track::read_from_path(path);
+            let path_str: String = row.get(1)?;
+            eprintln!("{}", path_str);
+            let track = Track::read_from_path(path_str);
             // track.name = row.get(0)?;
             // track.directory = row.get(2)?;
 
             Ok(track)
         })?;
 
-        for track in tracks {
-            eprintln!("Found track {}", track.unwrap().unwrap().file().unwrap());
-        }
-        todo!()
+        // for track in tracks {
+        //     eprintln!("Found track {:?}", track.unwrap().unwrap().name());
+        // }
+        Ok(())
     }
 }
