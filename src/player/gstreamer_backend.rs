ef07f19d1111533cd98dda4f4af90e52323bc038
tramhao <haoxinkui@gmail.com>
2022-06-27 02:19:33 +0800

    only work for the first track

--- src/player/gstreamer_backend.rs
+++ src/player/gstreamer_backend.rs
@@ -24,18 +24,24 @@
 use super::{PlayerMsg, PlayerTrait};
 use crate::config::Termusic;
 use anyhow::{anyhow, bail, Result};
-use fragile::Fragile;
+// use fragile::Fragile;
 use gst::ClockTime;
 use gstreamer as gst;
 use gstreamer::prelude::*;
-use gstreamer_player as gst_player;
 use std::cmp;
 // use std::rc::Rc;
-use glib::MainContext;
+// use glib::{FlagsClass, MainContext};
 use std::sync::mpsc::Sender;
 
+use glib::{FlagsClass, MainContext};
+use gst::Element;
+use std::sync::{Arc, Mutex};
+
+#[derive(Clone)]
 pub struct GStreamer {
-    player: gst_player::Player,
+    playbin: Element,
+    next_uri: Arc<Mutex<Option<String>>>,
+    main_tx: glib::Sender<PlayerMsg>,
     paused: bool,
     volume: i32,
     speed: f32,
@@ -46,68 +52,160 @@ pub struct GStreamer {
 impl GStreamer {
     pub fn new(config: &Termusic, message_tx: Sender<PlayerMsg>) -> Self {
         gst::init().expect("Couldn't initialize Gstreamer");
-        let dispatcher = gst_player::PlayerGMainContextSignalDispatcher::new(None);
-        let player = gst_player::Player::new(
-            None,
-            Some(&dispatcher.upcast::<gst_player::PlayerSignalDispatcher>()),
-        );
 
-        let volume = config.volume;
-        player.set_volume(f64::from(volume) / 100.0);
-        let speed = config.speed;
-        player.set_rate(speed.into());
+        let ctx = glib::MainContext::default();
+        let _guard = ctx.acquire();
+        let mainloop = glib::MainLoop::new(Some(&ctx), false);
 
-        let (tx, rx) = MainContext::channel(glib::PRIORITY_DEFAULT);
+        let playbin = gst::ElementFactory::make("playbin", None)
+            .expect("Unable to create the `playbin` element");
 
-        let tx_fragile = Fragile::new(tx);
-        player.connect_end_of_stream(move |_| {
-            eprintln!("ready to send eos");
-            tx_fragile.get().send(PlayerMsg::Eos).unwrap();
-        });
+        // Set flags to show Audio and Video but ignore Subtitles
+        let flags = playbin.property_value("flags");
+        let flags_class = FlagsClass::new(flags.type_()).unwrap();
+
+        let flags = flags_class
+            .builder_with_value(flags)
+            .unwrap()
+            .set_by_nick("audio")
+            .set_by_nick("video")
+            .unset_by_nick("text")
+            .build()
+            .unwrap();
+        playbin.set_property_from_value("flags", &flags);
+
+        // Asynchronous channel to communicate with main() with
+        let (main_tx, main_rx) = MainContext::channel(glib::Priority::default());
+        // Handle messages from GSTreamer bus
+        playbin
+            .bus()
+            .expect("Failed to get GStreamer message bus")
+            .add_watch(glib::clone!(@strong main_tx => move |_bus, msg| {
+                match msg.view() {
+                    gst::MessageView::Eos(_) =>
+                        main_tx.send(PlayerMsg::Eos)
+                        .expect("Unable to send message to main()"),
+                    gst::MessageView::Error(e) =>
+                        glib::g_debug!("song", "{}", e.error()),
+                        _ => (),
+                }
+                glib::Continue(true)
+            }))
+            .expect("Failed to connect to GStreamer message bus");
 
         let tx = message_tx.clone();
-        rx.attach(None, move |_action| {
-            tx.send(PlayerMsg::Eos).unwrap();
-            glib::Continue(true)
-        });
-        Self {
-            player,
+        main_rx.attach(
+            None,
+            glib::clone!(@strong mainloop => move |msg| {
+                // match msg {
+                    // BackendMessage::ReachedEndOfSong => {
+                    //     // Backend switches to the next track itself,
+                    //     // we just need to notify playlist about the change.
+                    //     playlist.lockk().next();
+                    // }
+                    // BackendMessage::ReachedEndOfPlaylist => {
+                    //     output.cleanup();
+                    //     mainloop.quit();
+                    // }
+                    // BackendMessage::RequestNextSong => {
+                    //     backend.enqueue(playlist.lockk().peek());
+                    // }
+                    // BackendMessage::State(state) => {
+                    //     output.refresh(state, &playlist.lockk())
+                    //         .ok(); // ignore any output errors
+                    // }
+                    // PlayerMsg::Eos => {
+                        tx.send(msg).unwrap();
+                    // }
+                // };
+                glib::Continue(true)
+            }),
+        );
+        // mainloop.run();
+
+        let volume = config.volume;
+        let speed = config.speed;
+
+        let this = Self {
+            playbin,
+            next_uri: Arc::new(Mutex::new(None)),
+            main_tx,
             paused: false,
             volume,
             speed,
             gapless: true,
-            tx: message_tx,
-        }
+            tx: message_tx.clone(),
+        };
+
+        // Switch to next song when reaching end of current track
+        // let tx = Fragile::new(message_tx.clone());
+        // this.playbin.connect(
+        //     "about-to-finish",
+        //     false,
+        //     glib::clone!(@strong this => move |_args| {
+        //        this.dequeue();
+        //        // tx.get().send(PlayerMsg::AboutToFinish).unwrap();
+        //        None
+        //     }),
+        // );
+
+        this
     }
     pub fn skip_one(&mut self) {
         self.tx.send(PlayerMsg::Eos).unwrap();
     }
     pub fn enqueue_next(&mut self, next_track: &str) {
-        self.player
-            .set_uri(Some(&format!("file:///{}", next_track)));
+        self.playbin
+            .set_property("uri", Some(&format!("file:///{}", next_track)));
     }
-    pub fn play(&mut self) {
-        self.player.stop();
-        self.player.play();
+    pub fn play(&mut self) {}
+
+    /// Sets the song to be played after the end of the current one
+    /// is reached. This is necessary for gapless playback.
+    pub fn enqueue(&mut self, track: &str) {
+        *self.next_uri.lock().unwrap() = Some(track.to_string());
+    }
+
+    /// Sets the playbin URI to `self.next_uri`, when it is not None.
+    /// This function is to be used from GStreamer playbin's
+    /// about-to-finish callback only.
+    pub fn dequeue(&self) {
+        if let Some(uri) = &*self.next_uri.lock().unwrap() {
+            self.playbin.set_property("uri", uri);
+            self.main_tx
+                .send(PlayerMsg::Eos)
+                .expect("Unable to send message to main()");
+        }
     }
 }
 
 impl PlayerTrait for GStreamer {
     fn add_and_play(&mut self, song_str: &str) {
-        self.player.set_uri(Some(&format!("file:///{}", song_str)));
-        self.paused = false;
+        self.playbin
+            .set_state(gst::State::Ready)
+            .expect("set gst state ready error.");
+        self.playbin
+            .set_property("uri", Some(&format!("file:///{}", song_str)));
+        self.playbin
+            .set_state(gst::State::Playing)
+            .expect("set gst state playing error");
+        // self.main_tx
+        //     .send(PlayerMsg::Eos)
+        //     .expect("Unable to send message to main()");
+        // self.player.set_uri(Some(&format!("file:///{}", song_str)));
+        // self.paused = false;
 
         self.play();
     }
 
     fn volume_up(&mut self) {
         self.volume = cmp::min(self.volume + 5, 100);
-        self.player.set_volume(f64::from(self.volume) / 100.0);
+        // self.player.set_volume(f64::from(self.volume) / 100.0);
     }
 
     fn volume_down(&mut self) {
         self.volume = cmp::max(self.volume - 5, 0);
-        self.player.set_volume(f64::from(self.volume) / 100.0);
+        // self.player.set_volume(f64::from(self.volume) / 100.0);
     }
 
     fn volume(&self) -> i32 {
@@ -121,21 +219,33 @@ impl PlayerTrait for GStreamer {
             volume = 0;
         }
         self.volume = volume;
-        self.player.set_volume(f64::from(volume) / 100.0);
+        // self.player.set_volume(f64::from(volume) / 100.0);
     }
 
     fn pause(&mut self) {
         self.paused = true;
-        self.player.pause();
+        // self.player.pause();
+        match self.playbin.current_state() {
+            _ => self
+                .playbin
+                .set_state(gst::State::Paused)
+                .expect("set gst state paused error"),
+        };
     }
 
     fn resume(&mut self) {
         self.paused = false;
-        self.player.play();
+        // self.player.play();
+        match self.playbin.current_state() {
+            _ => self
+                .playbin
+                .set_state(gst::State::Playing)
+                .expect("set gst state playing error in resume"),
+        };
     }
 
     fn is_paused(&self) -> bool {
-        self.paused
+        self.playbin.current_state() == gst::State::Paused
     }
 
     #[allow(clippy::cast_sign_loss)]
@@ -149,18 +259,31 @@ impl PlayerTrait for GStreamer {
             if seek_pos.cmp(&duration) == std::cmp::Ordering::Greater {
                 bail! {"exceed max length"};
             }
-            self.player.seek(ClockTime::from_seconds(seek_pos as u64));
+            let seek_pos = ClockTime::from_seconds(seek_pos as u64);
+            self.playbin
+                .seek_simple(gst::SeekFlags::FLUSH, seek_pos)
+                .ok(); // ignore any errors
         }
         Ok(())
     }
 
     #[allow(clippy::cast_precision_loss)]
     fn get_progress(&mut self) -> Result<(f64, i64, i64)> {
-        let time_pos = match self.player.position() {
+        let time_pos = match self
+            .playbin
+            .query_position::<gst::ClockTime>()
+            .unwrap_or_default()
+            .into()
+        {
             Some(t) => ClockTime::seconds(t).try_into().unwrap_or(0),
             None => 0_i64,
         };
-        let duration = match self.player.duration() {
+        let duration = match self
+            .playbin
+            .query_duration::<ClockTime>()
+            .unwrap_or_default()
+            .into()
+        {
             Some(d) => ClockTime::seconds(d).try_into().unwrap_or(0),
             None => 0_i64,
         };
@@ -179,7 +302,7 @@ impl PlayerTrait for GStreamer {
 
     fn set_speed(&mut self, speed: f32) {
         self.speed = speed;
-        self.player.set_rate(speed.into());
+        // self.player.set_rate(speed.into());
     }
 
     fn speed_up(&mut self) {
@@ -198,6 +321,15 @@ impl PlayerTrait for GStreamer {
         self.set_speed(speed);
     }
     fn stop(&mut self) {
-        self.player.stop();
+        self.playbin.set_state(gst::State::Null).ok();
+    }
+}
+
+impl Drop for GStreamer {
+    /// Cleans up GStreamer pipeline when `Backend` is dropped.
+    fn drop(&mut self) {
+        self.playbin
+            .set_state(gst::State::Null)
+            .expect("Unable to set the pipeline to the `Null` state");
     }
 }
