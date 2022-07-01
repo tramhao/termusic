404e99d3a9c11bc5ded8de5ea3e0491d21baa30b
tramhao <haoxinkui@gmail.com>
2022-07-01 15:27:28 +0800

    change mpv backend structure to listen for events

--- src/player/mpv_backend.rs
+++ src/player/mpv_backend.rs
@@ -23,22 +23,34 @@
  */
 use super::{PlayerMsg, PlayerTrait};
 use crate::config::Termusic;
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use libmpv::Mpv;
-// use libmpv::{events::Event, Format};
+use libmpv::{events::Event, Format};
 use std::cmp;
-use std::sync::mpsc::Sender;
+use std::sync::mpsc::{self, Receiver, Sender};
 
 pub struct MpvBackend {
-    player: Mpv,
+    // player: Mpv,
     volume: i32,
     speed: i32,
     pub gapless: bool,
-    tx: Sender<PlayerMsg>,
+    message_tx: Sender<PlayerMsg>,
+    command_tx: Sender<PlayerCmd>,
+}
+
+enum PlayerCmd {
+    Play(String),
+    QueueNext(String),
+    Volume(i64),
+    Pause,
+    Resume,
+    Speed(i32),
+    Stop,
 }
 
 impl MpvBackend {
     pub fn new(config: &Termusic, tx: Sender<PlayerMsg>) -> Self {
+        let (command_tx, command_rx): (Sender<PlayerCmd>, Receiver<PlayerCmd>) = mpsc::channel();
         let mpv = Mpv::new().expect("Couldn't initialize MpvHandlerBuilder");
         mpv.set_property("vo", "null")
             .expect("Couldn't set vo=null in libmpv");
@@ -52,104 +64,103 @@ impl MpvBackend {
         let gapless_setting = if gapless { "yes" } else { "no" };
         mpv.set_property("gapless-audio", gapless_setting)
             .expect("gapless setting failed");
+        // mpv.command(
+        //     "loadfile",
+        //     &[
+        //         &format!(
+        //             "\"{}\"",
+        //             "/home/tramhao/Music/mp3/misc/Gala乐队-Young For You.mp3"
+        //         ),
+        //         "replace",
+        //     ],
+        // )
+        // .expect("abc");
+        // eprintln!("add and play ok temp");
 
-        // let mut ev_ctx = mpv.create_event_context();
-        // ev_ctx
-        //     .disable_deprecated_events()
-        //     .expect("failed to disable deprecated events.");
-        // ev_ctx
-        //     .observe_property("volume", Format::Int64, 0)
-        //     .expect("failed to watch volume");
-        // ev_ctx
-        //     .observe_property("demuxer-cache-state", Format::Node, 0)
-        //     .expect("failed to watch demuxer cache");
-        // let message_tx = tx.clone();
-        // std::thread::spawn(move || loop {
-        //     let ev = ev_ctx.wait_event(600.).unwrap();
+        let message_tx = tx.clone();
+        std::thread::spawn(move || {
+            let mut ev_ctx = mpv.create_event_context();
+            ev_ctx
+                .disable_deprecated_events()
+                .expect("failed to disable deprecated events.");
+            ev_ctx
+                .observe_property("volume", Format::Int64, 0)
+                .expect("failed to watch volume");
+            loop {
+                if let Some(ev) = ev_ctx.wait_event(600.) {
+                    match ev {
+                        Ok(Event::EndFile(_)) => {
+                            // message_tx.send(PlayerMsg::Eos).unwrap();
+                        }
 
-        //     match ev {
-        //         Ok(Event::EndFile(r)) => {
-        //             println!("Exiting! Reason: {:?}", r);
-        //             break;
-        //         }
-
-        //         Ok(e) => println!("Event triggered: {:?}", e),
-        //         Err(e) => println!("Event errored: {:?}", e),
-        //     }
-        // });
+                        Ok(e) => eprintln!("Event triggered: {:?}", e),
+                        Err(e) => eprintln!("Event errored: {:?}", e),
+                    }
+                }
+                if let Ok(cmd) = command_rx.try_recv() {
+                    match cmd {
+                        // PlayerCmd::Eos => message_tx.send(PlayerMsg::Eos).unwrap(),
+                        PlayerCmd::Play(new) => {
+                            mpv.command("loadfile", &[&format!("\"{}\"", new), "replace"])
+                                .expect("Error loading file");
+                            eprintln!("add and play {} ok", new);
+                        }
+                        PlayerCmd::QueueNext(next) => {
+                            mpv.command("loadfile", &[&format!("\"{}\"", next), "append"])
+                                .expect("Error loading file");
+                        }
+                        PlayerCmd::Volume(volume) => {
+                            mpv.set_property("volume", volume)
+                                .expect("Error increase volume");
+                        }
+                        PlayerCmd::Pause => {
+                            mpv.set_property("pause", true)
+                                .expect("Toggling pause property");
+                        }
+                        PlayerCmd::Resume => {
+                            mpv.set_property("pause", false)
+                                .expect("Toggling pause property");
+                        }
+                        PlayerCmd::Speed(speed) => {
+                            mpv.set_property("speed", speed as f64 / 10.0).ok();
+                        }
+                        PlayerCmd::Stop => {
+                            mpv.command("stop", &[""]).expect("Error stop mpv player");
+                        }
+                    }
+                }
+            }
+        });
 
         Self {
-            player: mpv,
             volume,
             speed,
             gapless,
-            tx,
-            // current_item: None,
-            // next_item: None,
+            message_tx: tx,
+            command_tx,
         }
     }
 
-    pub fn enqueue_next(&mut self, new: &str) {
-        self.player
-            .command("loadfile", &[&format!("\"{}\"", new), "append"])
-            .expect("Error loading file");
+    pub fn enqueue_next(&mut self, next: &str) {
+        self.command_tx
+            .send(PlayerCmd::QueueNext(next.to_string()))
+            .ok();
     }
 
     fn queue_and_play(&mut self, new: &str) {
-        self.player
-            .command("loadfile", &[&format!("\"{}\"", new), "replace"])
-            .expect("Error loading file");
+        self.command_tx
+            .send(PlayerCmd::Play(new.to_string()))
+            .expect("failed to queue and play");
     }
 
     pub fn skip_one(&mut self) {
-        // self.player
-        //     .command("playlist_next", &["force"])
-        //     .expect("Error skip one file");
-        self.tx.send(PlayerMsg::Eos).unwrap();
+        self.message_tx.send(PlayerMsg::Eos).unwrap();
     }
-
-    // fn poll_events(&mut self) -> Result<bool> {
-    //     while let Some(ev) = self.player.wait_event(0.1) {
-    //         match ev {
-    //             mpv::Event::Shutdown | mpv::Event::Idle => {
-    //                 return Ok(false);
-    //             }
-    //             _ => log::debug!("mpv: {:?}", ev),
-    //         }
-    //     }
-    //     Ok(true)
-    // }
 }
 
 impl PlayerTrait for MpvBackend {
     fn add_and_play(&mut self, current_item: &str) {
-        // let gapless_setting = if self.gapless { "yes" } else { "no" };
-        // self.player
-        //     .set_property("gapless-audio", gapless_setting)
-        //     .expect("gapless setting failed");
-        // if self.next_item.is_none() {
-        //     self.stop();
-        // }
-
-        // self.current_item = Some(current_item.to_string());
-        // if self.current_item == self.next_item {
-        // This is for gapless playback
-        // if let Some(next) = next_item {
-        // self.next_item = Some(next.to_string());
-        // self.queue(next);
-        // }
-        // self.player
-        // .command("playlist_next", &["weak"])
-        // .command("playlist_next", &["force"])
-        // .expect("fail to go to next track");
-        // return;
-        // }
-
         self.queue_and_play(current_item);
-        // if let Some(next) = next_item {
-        //     self.next_item = Some(next.to_string());
-        //     self.queue(next);
-        // }
     }
 
     fn volume(&self) -> i32 {
@@ -158,16 +169,12 @@ impl PlayerTrait for MpvBackend {
 
     fn volume_up(&mut self) {
         self.volume = cmp::min(self.volume + 5, 100);
-        self.player
-            .set_property("volume", i64::from(self.volume))
-            .expect("Error increase volume");
+        self.set_volume(self.volume);
     }
 
     fn volume_down(&mut self) {
         self.volume = cmp::max(self.volume - 5, 0);
-        self.player
-            .set_property("volume", i64::from(self.volume))
-            .expect("Error decrease volume");
+        self.set_volume(self.volume);
     }
     fn set_volume(&mut self, mut volume: i32) {
         if volume > 100 {
@@ -176,48 +183,47 @@ impl PlayerTrait for MpvBackend {
             volume = 0;
         }
         self.volume = volume;
-        self.player
-            .set_property("volume", i64::from(self.volume))
-            .expect("Error setting volume");
+        self.command_tx
+            .send(PlayerCmd::Volume(i64::from(self.volume)))
+            .ok();
     }
 
     fn pause(&mut self) {
-        self.player
-            .set_property("pause", true)
-            .expect("Toggling pause property");
+        self.command_tx.send(PlayerCmd::Pause).ok();
     }
 
     fn resume(&mut self) {
-        self.player
-            .set_property("pause", false)
-            .expect("Toggling pause property");
+        self.command_tx.send(PlayerCmd::Resume).ok();
     }
 
     fn is_paused(&self) -> bool {
-        self.player
-            .get_property("pause")
-            .expect("wrong paused state")
+        // self.player
+        //     .get_property("pause")
+        //     .expect("wrong paused state")
+        true
     }
 
-    fn seek(&mut self, secs: i64) -> Result<()> {
-        match self
-            .player
-            .command("seek", &[&format!("\"{}\"", secs), "relative"])
-        {
-            Ok(r) => Ok(r),
-            Err(e) => Err(anyhow!(format!("Error in mpv: {}", e))),
-        }
+    fn seek(&mut self, _secs: i64) -> Result<()> {
+        // match self
+        //     .player
+        //     .command("seek", &[&format!("\"{}\"", secs), "relative"])
+        // {
+        //     Ok(r) => Ok(r),
+        //     Err(e) => Err(anyhow!(format!("Error in mpv: {}", e))),
+        // }
+        Ok(())
     }
 
     fn get_progress(&mut self) -> Result<(f64, i64, i64)> {
-        let percent_pos = self
-            .player
-            .get_property::<f64>("percent-pos")
-            .unwrap_or(0.0);
-        // let percent = percent_pos / 100_f64;
-        let time_pos = self.player.get_property::<i64>("time-pos").unwrap_or(0);
-        let duration = self.player.get_property::<i64>("duration").unwrap_or(0);
-        Ok((percent_pos, time_pos, duration))
+        // let percent_pos = self
+        //     .player
+        //     .get_property::<f64>("percent-pos")
+        //     .unwrap_or(0.0);
+        // // let percent = percent_pos / 100_f64;
+        // let time_pos = self.player.get_property::<i64>("time-pos").unwrap_or(0);
+        // let duration = self.player.get_property::<i64>("duration").unwrap_or(0);
+        // Ok((percent_pos, time_pos, duration))
+        Ok((0.5, 1, 100))
     }
 
     fn speed(&self) -> i32 {
@@ -226,7 +232,7 @@ impl PlayerTrait for MpvBackend {
 
     fn set_speed(&mut self, speed: i32) {
         self.speed = speed;
-        self.player.set_property("speed", speed as f64 / 10.0).ok();
+        self.command_tx.send(PlayerCmd::Speed(self.speed)).ok();
     }
 
     fn speed_up(&mut self) {
@@ -245,8 +251,6 @@ impl PlayerTrait for MpvBackend {
         self.set_speed(speed);
     }
     fn stop(&mut self) {
-        self.player
-            .command("stop", &[""])
-            .expect("Error stop mpv player");
+        self.command_tx.send(PlayerCmd::Stop).ok();
     }
 }
