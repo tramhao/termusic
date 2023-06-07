4ab8ecf4ff2abfefee39caab7ef6d8ce9540d60c
tramhao <haoxinkui@gmail.com>
2023-06-07 17:20:18 +0800

    server can play music

--- daemon/src/server.rs
+++ daemon/src/server.rs
@@ -1,6 +1,12 @@
+use anyhow::Result;
+use termusiclib::config::Settings;
 use termusicplayback::player::music_player_server::{MusicPlayer, MusicPlayerServer};
 use termusicplayback::player::{TogglePauseRequest, TogglePauseResponse};
+use termusicplayback::GeneralPlayer;
 use tonic::{transport::Server, Request, Response, Status};
+
+#[macro_use]
+extern crate log;
 
 #[derive(Debug, Default)]
 pub struct MusicPlayerService {}
@@ -18,10 +24,25 @@ impl MusicPlayer for MusicPlayerService {
     }
 }
 
+impl MusicPlayerService {
+    pub fn new() -> Result<Self> {
+        Ok(Self {})
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    lovely_env_logger::init_default();
+    info!("background thread start");
+
     let addr = "[::1]:50051".parse()?;
     let music_player_service: MusicPlayerService = MusicPlayerService::default();
+
+    let mut config = Settings::default();
+    config.load()?;
+    let mut player = GeneralPlayer::new(&config);
+
+    player.start_play();
 
     Server::builder()
         .add_service(MusicPlayerServer::new(music_player_service))
