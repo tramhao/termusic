14406ef31d6d0e36759146c8603d1b88a8f20a3c
hasezoey <hasezoey@gmail.com>
2023-11-25 14:13:05 +0100

    refactor(tui::main::execute_action): take "Settings" by reference
    as per clippy warning

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -53,7 +53,7 @@ async fn main() -> Result<()> {
     let config = get_config(&args)?;
 
     if let Some(action) = args.action {
-        execute_action(action, config);
+        execute_action(action, &config);
 
         return Ok(());
     }
@@ -143,7 +143,7 @@ fn get_path(dir: &str) -> Option<String> {
     music_dir
 }
 
-fn execute_action(action: cli::Action, config: Settings) {
+fn execute_action(action: cli::Action, config: &Settings) {
     match action {
         cli::Action::Import { file } => {
             println!("need to import from file {file}");
@@ -152,7 +152,7 @@ fn execute_action(action: cli::Action, config: Settings) {
             let db_path = utils::get_app_config_path();
 
             if let (Some(path_str), Ok(db_path)) = (path_str, db_path) {
-                if let Err(e) = podcast::import_from_opml(db_path.as_path(), &config, &path_str) {
+                if let Err(e) = podcast::import_from_opml(db_path.as_path(), config, &path_str) {
                     eprintln!("Error when import file {file}: {e}");
                 }
             }
