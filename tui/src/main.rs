84a0213fcd70346933b8d9cb1924c411a115b029
hasezoey <hasezoey@gmail.com>
2024-01-18 13:17:58 +0100

    fix(tui): start server with "--log-filecolor" if enabled on tui

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -102,6 +102,10 @@ async fn actual_main() -> Result<()> {
             server_args.push("--log-to-file");
         }
 
+        if args.log_options.file_color_log {
+            server_args.push("--log-filecolor");
+        }
+
         let proc = utils::spawn_process(&termusic_server_prog, false, false, &server_args)
             .unwrap_or_else(|_| panic!("Could not find {} binary", termusic_server_prog.display()));
 
