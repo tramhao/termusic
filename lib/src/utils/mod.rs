2ca3e4e71c881050d77aa6ccf379caa8aad5b9c0
tramhao <haoxinkui@gmail.com>
2024-03-17 04:44:17 +0800

    fix cargo test

--- lib/src/utils/mod.rs
+++ lib/src/utils/mod.rs
@@ -496,7 +496,7 @@ mod tests {
         let child: Rect = draw_area_in_relative(area, 75, 30);
         assert_eq!(child.x, 43);
         assert_eq!(child.y, 63);
-        assert_eq!(child.width, 271);
-        assert_eq!(child.height, 54);
+        assert_eq!(child.width, 272);
+        assert_eq!(child.height, 55);
     }
 }
