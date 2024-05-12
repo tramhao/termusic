cfc9ac922b22a9a62911d835afb6214bf4d2d8dc
hasezoey <hasezoey@gmail.com>
2024-05-12 15:06:04 +0200

    feat(lib::config): add "SharedServerSettings" & "SharedTuiSettings" types
    and corresponding "new_shared_*_settings" functions

--- lib/src/config/mod.rs
+++ lib/src/config/mod.rs
@@ -20,8 +20,28 @@ pub use tui_overlay::TuiOverlay;
 // Note that this (at least currently) unused in lib itself, but used in many of the other dependant crates (playback, server, tui)
 pub type SharedSettings = Arc<RwLock<Settings>>;
 
+/// The Server-Settings Object, but shared across many places
+// Note that this (at least currently) unused in lib itself, but used in many of the other dependant crates (playback, server, tui)
+pub type SharedServerSettings = Arc<RwLock<ServerOverlay>>;
+
+/// The Server-Settings Object, but shared across many places
+// Note that this (at least currently) unused in lib itself, but used in many of the other dependant crates (playback, server, tui)
+pub type SharedTuiSettings = Arc<RwLock<TuiOverlay>>;
+
 /// Create a new [`SharedSettings`] from just [`Settings`] without having to also depend on [`parking_lot`]
 #[inline]
 pub fn new_shared_settings(settings: Settings) -> SharedSettings {
+    Arc::new(RwLock::new(settings))
+}
+
+/// Create a new [`SharedServerSettings`] without having to also depend on [`parking_lot`]
+#[inline]
+pub fn new_shared_server_settings(settings: ServerOverlay) -> SharedServerSettings {
+    Arc::new(RwLock::new(settings))
+}
+
+/// Create a new [`SharedTuiSettings`] without having to also depend on [`parking_lot`]
+#[inline]
+pub fn new_shared_tui_settings(settings: TuiOverlay) -> SharedTuiSettings {
     Arc::new(RwLock::new(settings))
 }
