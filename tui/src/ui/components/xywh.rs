dc4b213eeb7e43322673aa7c365a5fcad5424919
tramhao <haoxinkui@gmail.com>
2024-08-22 16:09:40 +0800

    Fix a warning when only use rusty as feature

--- tui/src/ui/components/xywh.rs
+++ tui/src/ui/components/xywh.rs
@@ -22,7 +22,13 @@
  * SOFTWARE.
  */
 use crate::ui::model::{Model, ViuerSupported};
-use anyhow::{Context, Result};
+#[cfg(any(
+    feature = "cover-viuer-iterm",
+    feature = "cover-viuer-kitty",
+    feature = "cover-viuer-sixel"
+))]
+use anyhow::Context;
+use anyhow::Result;
 use bytes::Buf;
 use image::io::Reader as ImageReader;
 use image::DynamicImage;
