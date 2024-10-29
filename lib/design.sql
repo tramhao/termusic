f894406f4029f05890851d892ae4ddc0ed3c3b23
hasezoey <hasezoey@gmail.com>
2024-10-29 15:04:04 +0100

    chore(design.sql): WIP database design

---
+++ lib/design.sql
@@ -0,0 +1,128 @@
+--- SECTION: Config
+
+-- table for extra information in a key-value style
+-- examples ("key: val_format"): "db_created_at: DATE", "last_used_version: TERMUSIC_VERSION"
+CREATE TABLE IF NOT EXISTS config(
+    key TEXT NOT NULL PRIMARY KEY,
+    value TEXT NOT NULL,
+)
+
+--- SECTION: local music files
+
+-- the table for all local music files to use as reference
+CREATE TABLE IF NOT EXISTS files(
+    id INTEGER PRIMARY KEY,
+    -- file cannot be null, as it is used as a identifier
+    file TEXT NOT NULL UNIQUE,
+    -- duration parsing may fail or somehow not be available
+    duration INTEGER,
+    -- last known played position, if NULL should assume 0
+    last_position INTEGER,
+    -- the date the file was first added to the database
+    added_at DATE
+);
+
+-- single metadata for a file
+CREATE TABLE IF NOT EXISTS files_metadata(
+    id INTEGER PRIMARY KEY,
+    -- the file this metadata is for; if the related file is dropped, drop this too
+    file INTEGER NOT NULL REFERENCES files(id) ON DELETE CASCADE,
+    -- can be null if not present or cannot be parsed
+    title TEXT,
+    -- can be null if not present or cannot be parsed
+    genre TEXT
+);
+
+-- the table for all artists
+-- this is so that tracks with "ArtistA feat. ArtistB" can be searched for either
+CREATE TABLE IF NOT EXISTS artists(
+    id INTEGER PRIMARY KEY,
+    -- artist is used as a identifier, if not present, it should not be added to the database
+    artist TEXT NOT NULL UNIQUE,
+    -- the date this artist was added to the database
+    added_at DATE
+);
+
+-- relation table for a file's artist
+-- entry will get deleted if the artist is dropped or the file is dropped
+CREATE TABLE IF NOT EXISTS files_artists(
+    id INTEGER PRIMARY KEY,
+    file INTEGER NOT NULL REFERENCES files(id) ON DELETE CASCADE,
+    artist INTEGER NOT NULL REFERENCES artists(id) ON DELETE CASCADE,
+);
+
+-- the table for all albums
+-- Note: not unique, there *could* be ones with the same name
+CREATE TABLE IF NOT EXISTS albums(
+    id INTEGER PRIMARY KEY,
+    -- can be null if failed to parse, or is in a album but does not have a name for whatever reason
+    -- TODO: should we force "NOT NULL"?
+    title TEXT,
+);
+
+-- relation table for a album's artist
+-- this is so that albums with "ArtistA feat. ArtistB" can be searched for either
+-- entry will get deleted if either the artist or the album get dropped
+CREATE TABLE IF NOT EXISTS albums_artist(
+    id INTEGER PRIMARY KEY,
+    album INTEGER NOT NULL REFERENCES albums(id) ON DELETE CASCADE,
+    artist INTEGER NOT NULL REFERENCES artists(id) ON DELETE CASCADE
+);
+
+-- relation table for a file which belongs to a album
+-- TODO: should this maybe get integrated to the "files" table as a file can really only belong to one album?
+CREATE TABLE IF NOT EXISTS files_album(
+    id INTEGER PRIMARY KEY,
+    file INTEGER NOT NULL REFERENCES files(id) ON DELETE CASCADE,
+    album INTEGER NOT NULL REFERENCES albums(id) ON DELETE CASCADE
+);
+
+--- SECTION: podcasts
+
+-- the table for all top-level podcasts
+CREATE TABLE IF NOT EXISTS podcasts(
+    id INTEGER PRIMARY KEY,
+    -- as podcasts are not just local, a url is required
+    url TEXT NOT NULL UNIQUE,
+    -- refuse podcasts which do not have a title
+    title TEXT NOT NULL,
+    -- the author could not be present and is not necessary
+    author TEXT,
+    explicit BOOLEAN,
+    -- optional image url
+    image_url TEXT,
+    -- the time the podcast was last checked for updates
+    last_checked DATE NOT NULL,
+    -- optional description of the podcast
+    description TEXT,
+);
+
+-- the table for episodes of a podcast
+-- NOTE: local episode files should be named with the id of "podcast_episodes" and be auto-discovered
+CREATE TABLE IF NOT EXISTS podcast_episodes(
+    id INTEGER PRIMARY KEY,
+    -- the podcast this episode is for, delete on podcast delete
+    podcast INTEGER NOT NULL REFERENCES podcasts(id) ON DELETE CASCADE,
+    -- preferably this should always be present, but not all feeds may have it
+    guid TEXT,
+    -- the title is required, if not present the app should auto-fill with something
+    title TEXT NOT NULL,
+    -- the url to the individual file for direct play
+    url TEXT NOT NULL,
+    -- indicator for if the episode had already been fully played
+    played BOOLEAN NOT NULL,
+    -- indicator for if the episode should be hidden
+    hidden BOOLEAN NOT NULL,
+    -- last known played position, if NULL should assume 0
+    last_position INTEGER,
+    -- optional duration, preferably should always be present
+    duration INTEGER,
+    -- optional description of the episode
+    description TEXT
+);
+
+-- Open questions:
+-- 1: should pocast author and music artist link to the same place or be separate?
+-- 2: should "created_at" and "modified_at" for local music files also be stored for quick search?
+-- 3: should "files.file" be split to "directory, file_stem, ext" instead of just the full path?
+-- 4: should we do some kind of design where there is one single "last_position" table for both music and podcasts?
