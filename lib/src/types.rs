0f8e1743d4291a2e0fe94a236ac643fcabf3824b
hasezoey <hasezoey@gmail.com>
2025-06-07 17:46:11 +0200

    refactor(lib): move "const_unknown" values to "types"
    also change "_ALBUM" to "Unknown Album" instead of "empty"

--- lib/src/types.rs
+++ lib/src/types.rs
@@ -646,6 +646,18 @@ impl SearchCriteria {
     }
 }
 
+/// Constant strings for Unknown values
+pub mod const_unknown {
+    use crate::const_str;
+
+    const_str! {
+        UNKNOWN_ARTIST "Unknown Artist",
+        UNKNOWN_TITLE "Unknown Title",
+        UNKNOWN_ALBUM "Unknown Album",
+        UNKNOWN_FILE "Unknown File",
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::types::{IdKey, KFGLOBAL_FOCUS_ORDER, KFOTHER_FOCUS_ORDER};
