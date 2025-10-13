tramhao/termusic

commit 5f037bc281e96734e3d5e0275c6f5936fd11892a

refactor: move "lib::utils::spawn_process" to "tui"
As it is only used there

tui/src/main.rs
@@ -1,5 +1,7 @@
+use std::ffi::OsStr;
 use std::net::SocketAddr;
 use std::path::PathBuf;
+use std::process::Stdio;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{Arc, OnceLock};
 use std::time::{Duration, Instant};
@@ -20,7 +22,7 @@ use termusiclib::config::{
 use termusiclib::player::music_player_client::MusicPlayerClient;
 use termusiclib::{podcast, utils};
 use tokio::io::AsyncReadExt;
-use tokio::process::Child;
+use tokio::process::{Child, Command};
 use tokio::sync::RwLock;
 use tokio::task::AbortHandle;
 use tokio_util::sync::CancellationToken;
@@ -205,13 +207,34 @@ fn launch_server(args: &cli::Args) -> Result<Child> {
 
     // server can stay around after client exits (if supported by the system)
     #[allow(clippy::zombie_processes)]
-    let proc =
-        utils::spawn_process(&termusic_server_prog, false, true, &server_args).context(format!(
-            "Could not start binary \"{}\"",
-            termusic_server_prog.display()
-        ))?;
+    let proc = spawn_process(&termusic_server_prog, false, &server_args).context(format!(
+        "Could not start binary \"{}\"",
+        termusic_server_prog.display()
+    ))?;
 
     Ok(proc)
+}
+
+/// Spawn a detached process
+/// # Panics
+/// panics when spawn server failed
+fn spawn_process<A: IntoIterator<Item = S> + Clone, S: AsRef<OsStr>>(
+    prog: &Path,
+    piped: bool,
+    args: A,
+) -> std::io::Result<Child> {
+    let mut cmd = Command::new(prog);
+    cmd.stdin(Stdio::null());
+    if piped {
+        cmd.stdout(Stdio::piped());
+        cmd.stderr(Stdio::piped());
+    } else {
+        cmd.stdout(Stdio::null());
+        cmd.stderr(Stdio::null());
+    }
+
+    cmd.args(args);
+    cmd.spawn()
 }
 
 /// Try to find a active server process, returning its [`Pid`].

lib/src/utils.rs
@@ -2,12 +2,10 @@ use std::borrow::Cow;
 use std::ffi::OsStr;
 use std::iter::FusedIterator;
 use std::path::{Path, PathBuf};
-use std::process::Stdio;
 
 use anyhow::{Context, Result, anyhow};
 use pinyin::ToPinyin;
 use rand::Rng;
-use tokio::process::{Child, Command};
 use unicode_segmentation::UnicodeSegmentation;
 
 use crate::config::ServerOverlay;
@@ -190,35 +188,6 @@ impl StringUtils for String {
     fn grapheme_len(&self) -> usize {
         self.as_str().grapheme_len()
     }
-}
-
-/// Spawn a detached process
-/// # Panics
-/// panics when spawn server failed
-pub fn spawn_process<A: IntoIterator<Item = S> + Clone, S: AsRef<OsStr>>(
-    prog: &Path,
-    superuser: bool,
-    shout_output: bool,
-    args: A,
-) -> std::io::Result<Child> {
-    let mut cmd = if superuser {
-        let mut cmd_t = Command::new("sudo");
-        cmd_t.arg(prog);
-        cmd_t
-    } else {
-        Command::new(prog)
-    };
-    cmd.stdin(Stdio::null());
-    if shout_output {
-        cmd.stdout(Stdio::piped());
-        cmd.stderr(Stdio::piped());
-    } else {
-        cmd.stdout(Stdio::null());
-        cmd.stderr(Stdio::null());
-    }
-
-    cmd.args(args);
-    cmd.spawn()
 }
 
 /// Absolutize a given path with the current working directory.

commit 81eb9abe8f93386ae85bfc01235ca28af25b9b75

fix(tui::launch_server): actually create with pipes
broken since 5f037bc281e96734e3d5e0275c6f5936fd11892a / #583

tui/src/main.rs
@@ -209,7 +209,7 @@ fn launch_server(args: &cli::Args) -> Result<Child> {
 
     // server can stay around after client exits (if supported by the system)
     #[allow(clippy::zombie_processes)]
-    let proc = spawn_process(&termusic_server_prog, false, &server_args).context(format!(
+    let proc = spawn_process(&termusic_server_prog, true, &server_args).context(format!(
         "Could not start binary \"{}\"",
         termusic_server_prog.display()
     ))?;