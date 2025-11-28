tramhao/termusic

commit 2e289e7a0e58b026abe18e855e2c31f4f75b0430

refactor(tui::ui::components::music_library): handle "TreeNodeReady" within component itself

tui/src/ui/components/music_library.rs
@@ -5,20 +5,19 @@ use anyhow::{Context, Result, bail};
 use termusiclib::config::SharedTuiSettings;
 use termusiclib::config::v2::server::ScanDepth;
 use termusiclib::config::v2::server::config_extra::ServerConfigVersionedDefaulted;
-use tui_realm_treeview::{Node, TREE_CMD_CLOSE, TREE_CMD_OPEN, TREE_INITIAL_NODE, Tree, TreeView};
+use tui_realm_treeview::{Node, TREE_CMD_CLOSE, TREE_CMD_OPEN, Tree, TreeView};
 use tuirealm::command::{Cmd, CmdResult, Direction, Position};
 use tuirealm::event::{Key, KeyEvent, KeyModifiers};
 use tuirealm::props::{Alignment, BorderType, Borders, TableBuilder, TextSpan};
 use tuirealm::{
-    AttrValue, Attribute, Component, Event, MockComponent, State, StateValue, Sub, SubClause,
-    SubEventClause,
+    Component, Event, MockComponent, State, StateValue, Sub, SubClause, SubEventClause,
 };
 
 use crate::ui::ids::Id;
 use crate::ui::model::{DownloadTracker, Model, TxToMain, UserEvent};
 use crate::ui::msg::{
-    DeleteConfirmMsg, GSMsg, LIMsg, LINodeReadySub, LIReloadData, LIReloadPathData, Msg, PLMsg,
-    RecVec, TEMsg, YSMsg,
+    DeleteConfirmMsg, GSMsg, LIMsg, LINodeReady, LINodeReadySub, LIReloadData, LIReloadPathData,
+    Msg, PLMsg, RecVec, TEMsg, YSMsg,
 };
 use crate::ui::tui_cmd::TuiCmd;
 use crate::utils::get_pin_yin;
@@ -354,6 +353,31 @@ impl MusicLibrary {
         );
     }
 
+    /// Apply the given data as the root of the tree, resetting the state of the tree.
+    ///
+    /// This will always replace the root of the tree.
+    fn handle_ready(&mut self, data: LINodeReady) -> Msg {
+        let vec = data.0;
+        let initial_node = data.1;
+
+        let initial_node =
+            initial_node.or(self.component.tree_state().selected().map(String::from));
+
+        let tree = Tree::new(recvec_to_node(vec));
+
+        // There is no "clear" method for state in Treeview currently, so we have to
+        // entirely replace the Treeview component. The simplest way is to just replace via a new instance.
+        *self = Self::new(
+            &tree,
+            initial_node,
+            self.config.clone(),
+            self.tx_to_main.clone(),
+            self.download_tracker.clone(),
+        );
+
+        Msg::ForceRedraw
+    }
+
     /// Apply the given data at the path the data is, potentially without changing root.
     ///
     /// This will replace the root if the given data is starting at the root path.
@@ -516,6 +540,7 @@ impl MusicLibrary {
                 self.handle_reload_at(data);
                 None
             }
+            LIMsg::TreeNodeReady(data) => Some(self.handle_ready(data)),
             LIMsg::TreeNodeReadySub(data) => self.handle_ready_sub(data),
             _ => None,
         }
@@ -756,7 +781,9 @@ fn library_scan<P: Into<PathBuf>>(
     focus_node: Option<String>,
 ) {
     library_scan_cb(download_tracker, path, depth, move |vec| {
-        let _ = tx.send(Msg::Library(LIMsg::TreeNodeReady(vec, focus_node)));
+        let _ = tx.send(Msg::Library(LIMsg::TreeNodeReady(LINodeReady(
+            vec, focus_node,
+        ))));
     });
 }
 
@@ -850,6 +877,12 @@ impl Model {
                 SubClause::Always,
             ),
             Sub::new(
+                SubEventClause::User(UserEvent::Forward(Msg::Library(LIMsg::TreeNodeReady(
+                    LINodeReady(bogus_recvec.clone(), None),
+                )))),
+                SubClause::Always,
+            ),
+            Sub::new(
                 SubEventClause::User(UserEvent::Forward(Msg::Library(LIMsg::TreeNodeReadySub(
                     LINodeReadySub(bogus_recvec, PathBuf::new()),
                 )))),
@@ -903,44 +936,6 @@ impl Model {
         let _ = self
             .tx_to_main
             .send(Msg::Library(LIMsg::ReloadPath(LIReloadPathData(path))));
-    }
-
-    /// Apply the given [`RecVec`] as a tree
-    pub fn library_apply_as_tree(
-        &mut self,
-        msg: RecVec<PathBuf, String>,
-        focus_node: Option<String>,
-    ) {
-        let root_node = recvec_to_node(msg);
-
-        let old_current_node = match self.app.state(&Id::Library).ok().unwrap() {
-            State::One(StateValue::String(id)) => Some(id),
-            _ => None,
-        };
-
-        let tree = Tree::new(root_node);
-
-        // remount preserves focus
-        let _ = self.app.remount(
-            Id::Library,
-            Box::new(MusicLibrary::new(
-                &tree,
-                old_current_node,
-                self.config_tui.clone(),
-                self.tx_to_main.clone(),
-                self.download_tracker.clone(),
-            )),
-            Vec::new(),
-        );
-
-        // focus the specified node
-        if let Some(id) = focus_node {
-            let _ = self.app.attr(
-                &Id::Library,
-                Attribute::Custom(TREE_INITIAL_NODE),
-                AttrValue::String(id),
-            );
-        }
     }
 
     /// Show a deletion confirmation for the currently selected node.

tui/src/ui/model/update.rs
@@ -651,14 +651,12 @@ impl Model {
                     self.mount_error_popup(e.context("library remove root"));
                 }
             }
-            LIMsg::TreeNodeReady(vec, focus_node) => {
-                self.library_apply_as_tree(vec, focus_node);
-            }
 
             // handled by the component
             LIMsg::Reload(_data) => (),
             LIMsg::ReloadPath(_data) => (),
-            LIMsg::TreeNodeReadySub(_vec) => (),
+            LIMsg::TreeNodeReady(_data) => (),
+            LIMsg::TreeNodeReadySub(_data) => (),
         }
     }
 

tui/src/ui/msg.rs
@@ -189,6 +189,21 @@ impl PartialEq for LIReloadPathData {
     }
 }
 
+/// Data for [`LIMsg::TreeNodeReady`].
+///
+/// The path given is the one that is reloaded and also optionally focused.
+///
+/// Contains: `vec_data, focus_node`
+#[derive(Clone, Debug, Eq)]
+pub struct LINodeReady(pub RecVec<PathBuf, String>, pub Option<String>);
+
+/// `PartialEq` is only used for subscriptions.
+impl PartialEq for LINodeReady {
+    fn eq(&self, _other: &Self) -> bool {
+        true
+    }
+}
+
 /// Data for [`LIMsg::TreeNodeReadySub`].
 ///
 /// The path given is the one that is reloaded and also focused.
@@ -227,9 +242,7 @@ pub enum LIMsg {
     /// A requested node is ready from loading.
     ///
     /// Replaces the tree root.
-    ///
-    /// `(Tree, FocusNode)`
-    TreeNodeReady(RecVec<PathBuf, String>, Option<String>),
+    TreeNodeReady(LINodeReady),
     /// A requested node is ready to be reloaded within the current tree.
     ///
     /// Does not replace the tree root.

commit 24e87c7a5ddce28aa8f6163f479948a4f3c6a1db

fix(tui::ui::components::music_library::handle_ready): preserve focus
Broken since 2e289e7a0e58b026abe18e855e2c31f4f75b0430

tui/src/ui/components/music_library.rs
@@ -10,7 +10,7 @@ use tuirealm::command::{Cmd, CmdResult, Direction, Position};
 use tuirealm::event::{Key, KeyEvent, KeyModifiers};
 use tuirealm::props::{Alignment, BorderType, Borders, TableBuilder, TextSpan};
 use tuirealm::{
-    Component, Event, MockComponent, State, StateValue, Sub, SubClause, SubEventClause,
+    Attribute, Component, Event, MockComponent, State, StateValue, Sub, SubClause, SubEventClause,
 };
 
 use crate::ui::ids::Id;
@@ -416,6 +416,8 @@ impl MusicLibrary {
 
         let tree = Tree::new(recvec_to_node(vec));
 
+        let focus = self.component.query(Attribute::Focus);
+
         // There is no "clear" method for state in Treeview currently, so we have to
         // entirely replace the Treeview component. The simplest way is to just replace via a new instance.
         *self = Self::new(
@@ -425,6 +427,10 @@ impl MusicLibrary {
             self.tx_to_main.clone(),
             self.download_tracker.clone(),
         );
+
+        if let Some(focus) = focus {
+            self.attr(Attribute::Focus, focus);
+        }
 
         Msg::ForceRedraw
     }