17cc018c898e5acd8d8436158873d01d4e8472d3
hasezoey <hasezoey@gmail.com>
2025-12-01 14:38:38 +0100

    refactor(tui::ui::components::orx_music_library): add scanner functions

--- tui/src/ui/components/orx_music_library/music_library.rs
+++ tui/src/ui/components/orx_music_library/music_library.rs
@@ -2,11 +2,17 @@
 
 use std::{
     cell::{OnceCell, RefCell},
-    path::PathBuf,
+    num::NonZeroUsize,
+    path::{Path, PathBuf},
 };
 
+use anyhow::Result;
+use termusiclib::config::{SharedTuiSettings, TuiOverlay, v2::server::ScanDepth};
 use tuirealm::{
-    props::Style,
+    Component, Event, MockComponent, State, StateValue,
+    command::{Cmd, CmdResult, Direction, Position},
+    event::{Key, KeyEvent, KeyModifiers},
+    props::{Alignment, BorderType, Borders, Style},
     ratatui::{
         buffer::Buffer,
         layout::Rect,
@@ -14,8 +20,20 @@ use tuirealm::{
     },
 };
 use tuirealm_orx_tree::{
-    types::NodeValue,
+    NodeRef,
+    component::TreeView,
+    traversal::{Dfs, OverNode, Traverser},
+    types::{NodeValue, Tree},
     widget::{CHILD_INDICATOR_LENGTH, RenderIndicator, calc_area_for_value},
+};
+
+use crate::ui::{
+    components::orx_music_library::scanner::library_scan,
+    model::{DownloadTracker, TxToMain, UserEvent},
+    msg::{
+        GSMsg, LIMsg, LINodeReady, LINodeReadySub, LIReloadData, LIReloadPathData, Msg, PLMsg,
+        TEMsg, YSMsg,
+    },
 };
 
 /// Data stored in a node in the [`NewMusicLibraryComponent`]'s tree.
@@ -107,5 +125,430 @@ impl NodeValue for MusicLibData {
         }
 
         self.render(buf, area, offset, style);
+    }
+}
+
+const LOADING_TREE_TEXT: &str = "Loading...";
+
+#[derive(Debug, MockComponent)]
+pub struct NewMusicLibraryComponent {
+    component: TreeView<MusicLibData>,
+    config: SharedTuiSettings,
+}
+
+impl NewMusicLibraryComponent {
+    fn get_inner_comp(config: &TuiOverlay) -> TreeView<MusicLibData> {
+        TreeView::<MusicLibData>::default()
+            .background(config.settings.theme.library_background())
+            .foreground(config.settings.theme.library_foreground())
+            .border(
+                Borders::default()
+                    .color(config.settings.theme.library_border())
+                    .modifiers(BorderType::Rounded),
+            )
+            .indent_size(2)
+            .scroll_step_horizontal(NonZeroUsize::new(2).unwrap())
+            .title(" Library ", Alignment::Left)
+            .highlight_color(config.settings.theme.library_highlight())
+            .highlight_symbol(&config.settings.theme.style.library.highlight_symbol)
+            .empty_tree_text(LOADING_TREE_TEXT)
+        // .on_open(|event, idx, tree| {
+        //     debug!("on_open {:#?}", (event, idx /* , tree */));
+
+        //     if tree.get_root().is_some_and(|v| v.idx() == idx) {
+        //         return;
+        //     }
+
+        //     let mut node = tree.get_node_mut(&idx).unwrap();
+        //     let mut traverser = Dfs::<OverData>::new();
+        //     // inital tree walker
+        //     let walker = node.walk_mut_with(&mut traverser);
+        //     // filter only for leafs
+        //     let walker = walker.skip(1);
+        //     // filter only directories & not loading
+        //     let walker = walker.filter(|v| v.is_dir && !v.is_loading);
+        //     for node in walker {
+        //         debug!("on_open found: {:#?}", node.path.file_name().unwrap());
+        //         node.is_loading = true;
+        //         scan_path_parent(tx, download_tracker, path, depth, parent);
+        //     }
+        // })
+    }
+
+    /// Get a new empty instance, which shows "Loading..." while empty.
+    pub fn new_loading(config: SharedTuiSettings) -> Self {
+        let component = {
+            let config = config.read();
+
+            Self::get_inner_comp(&config)
+        };
+
+        Self { component, config }
+    }
+
+    /// Create a new instance, with a tree already set.
+    pub fn new(
+        tree: Tree<MusicLibData>,
+        config: SharedTuiSettings,
+        tx: TxToMain,
+        tracker: DownloadTracker,
+    ) -> Self {
+        let mut this = Self::new_loading(config);
+
+        this.component = this.component.tree(tree).on_open(move |event, idx, tree| {
+            debug!("on_open {:#?}", (event, idx /* , tree */));
+
+            if tree.get_root().is_some_and(|v| v.idx() == idx) {
+                return;
+            }
+
+            let node = tree.get_node_mut(&idx).unwrap();
+            let mut traverser = Dfs::<OverNode>::new();
+            // inital tree walker
+            let walker = node.walk_with(&mut traverser);
+            // filter only for leafs
+            let walker = walker.skip(1);
+            // filter only directories & not loading
+            let walker = walker.filter(|v| v.data().is_dir && !(*v.data().is_loading.borrow()));
+            for node in walker {
+                let idx = node.idx();
+                let data = node.data();
+                debug!("on_open found: {:#?}", data.path.file_name().unwrap());
+                *data.is_loading.borrow_mut() = true;
+                library_scan(
+                    tracker.clone(),
+                    &data.path,
+                    ScanDepth::Limited(2),
+                    tx.clone(),
+                    Some(idx),
+                );
+            }
+        });
+
+        debug!("what {this:#?}");
+
+        this
+    }
+
+    /// Trigger a load with a message to change the tree root to the given path.
+    ///
+    /// This will make the given path(which will be the root node) the focused node.
+    ///
+    /// This will send a [`LIMsg::TreeNodeReady`] and change the root to `path`.
+    fn trigger_load_stepinto<P: Into<PathBuf>>(&self, path: P) {
+        todo!();
+        // library_scan(
+        //     self.download_tracker.clone(),
+        //     path,
+        //     ScanDepth::Limited(2),
+        //     self.tx_to_main.clone(),
+        //     None,
+        // );
+    }
+
+    /// Trigger a load with a message to change the tree root to the given path.
+    ///
+    /// This will make the current tree root be the new focused node.
+    ///
+    /// This will send a [`LIMsg::TreeNodeReady`] and change the root to `path`.
+    fn trigger_load_with_focus<P: Into<PathBuf>>(&self, scan_path: P, focus_node: Option<PathBuf>) {
+        todo!();
+        // let path = scan_path.into();
+        // library_scan(
+        //     self.download_tracker.clone(),
+        //     path,
+        //     ScanDepth::Limited(2),
+        //     self.tx_to_main.clone(),
+        //     focus_node,
+        // );
+    }
+
+    /// Store the currently selected node as yanked (for pasting with [`Self::paste`]).
+    fn yank(&mut self) {
+        todo!();
+    }
+
+    /// Paste the previously yanked node in the currently selected node if it is a directory, otherwise in its parent.
+    fn paste(&mut self) -> Result<Option<LIMsg>> {
+        todo!();
+    }
+
+    /// Get the current root node's path, if there is one.
+    fn get_root_path(&self) -> Option<&Path> {
+        self.component
+            .get_tree()
+            .get_root()
+            .map(|v| v.data().path.as_path())
+    }
+
+    /// Get the current selected node's path, if there is one.
+    fn get_selected_path(&self) -> Option<&Path> {
+        self.component
+            .get_current_selected_node()
+            .map(|v| v.data().path.as_path())
+    }
+
+    /// Also known as going up in the tree
+    fn handle_left_key(&mut self) -> (CmdResult, Option<Msg>) {
+        todo!();
+    }
+
+    /// Also known as going down the tree / adding file to playlist
+    fn handle_right_key(&mut self) -> (CmdResult, Option<Msg>) {
+        todo!();
+    }
+
+    /// Handle sending a request to delete the currently selected node.
+    fn handle_delete(&mut self) -> Msg {
+        todo!();
+    }
+
+    /// Handle a full reload / potential change of the current tree root.
+    ///
+    /// Also changes focus, if requested.
+    fn handle_full_reload(&mut self, data: LIReloadData) -> Msg {
+        todo!();
+    }
+
+    /// Handle reloading of the given path, potentially without changing root, but also change focus.
+    ///
+    /// If necessary, load all paths in-between.
+    fn handle_reload_at(&mut self, data: LIReloadPathData) {
+        todo!();
+    }
+
+    /// Apply the given data as the root of the tree, resetting the state of the tree.
+    ///
+    /// This will always replace the root of the tree.
+    fn handle_ready(&mut self, data: LINodeReady) -> Msg {
+        todo!();
+    }
+
+    /// Apply the given data at the path the data is, potentially without changing root.
+    ///
+    /// This will replace the root if the given data is starting at the root path.
+    fn handle_ready_sub(&mut self, data: LINodeReadySub) -> Option<Msg> {
+        todo!();
+    }
+
+    /// Handle all custom messages.
+    fn handle_user_events(&mut self, ev: LIMsg) -> Option<Msg> {
+        // handle subscriptions
+        match ev {
+            LIMsg::Reload(data) => Some(self.handle_full_reload(data)),
+            LIMsg::ReloadPath(data) => {
+                self.handle_reload_at(data);
+                None
+            }
+            LIMsg::TreeNodeReady(data) => Some(self.handle_ready(data)),
+            LIMsg::TreeNodeReadySub(data) => self.handle_ready_sub(data),
+            _ => None,
+        }
+    }
+}
+
+impl Component<Msg, UserEvent> for NewMusicLibraryComponent {
+    #[allow(clippy::too_many_lines)]
+    #[allow(unsafe_code)]
+    fn on(&mut self, ev: Event<UserEvent>) -> Option<Msg> {
+        if let Event::User(UserEvent::Forward(Msg::Library(ev))) = ev {
+            return self.handle_user_events(ev);
+        }
+
+        let config = self.config.clone();
+        let keys = &config.read().settings.keys;
+        let result = match ev {
+            // selection
+            Event::Keyboard(keyevent) if keyevent == keys.navigation_keys.left.get() => {
+                match self.handle_left_key() {
+                    (_, Some(msg)) => return Some(msg),
+                    (cmdresult, None) => cmdresult,
+                }
+            }
+            Event::Keyboard(keyevent) if keyevent == keys.navigation_keys.right.get() => {
+                match self.handle_right_key() {
+                    (_, Some(msg)) => return Some(msg),
+                    (cmdresult, None) => cmdresult,
+                }
+            }
+            Event::Keyboard(keyevent) if keyevent == keys.navigation_keys.down.get() => {
+                self.perform(Cmd::Move(Direction::Down))
+            }
+            Event::Keyboard(keyevent) if keyevent == keys.navigation_keys.up.get() => {
+                self.perform(Cmd::Move(Direction::Up))
+            }
+            Event::Keyboard(KeyEvent {
+                code: Key::Left,
+                modifiers: KeyModifiers::NONE,
+            }) => match self.handle_left_key() {
+                (_, Some(msg)) => return Some(msg),
+                (cmdresult, None) => cmdresult,
+            },
+            Event::Keyboard(KeyEvent {
+                code: Key::Right,
+                modifiers: KeyModifiers::NONE,
+            }) => match self.handle_right_key() {
+                (_, Some(msg)) => return Some(msg),
+                (cmdresult, None) => cmdresult,
+            },
+            Event::Keyboard(KeyEvent {
+                code: Key::Down,
+                modifiers: KeyModifiers::NONE,
+            }) => self.perform(Cmd::Move(Direction::Down)),
+            Event::Keyboard(KeyEvent {
+                code: Key::Up,
+                modifiers: KeyModifiers::NONE,
+            }) => self.perform(Cmd::Move(Direction::Up)),
+
+            // quick selection movement
+            Event::Keyboard(KeyEvent {
+                code: Key::PageDown,
+                modifiers: KeyModifiers::NONE,
+            }) => self.perform(Cmd::Scroll(Direction::Down)),
+            Event::Keyboard(KeyEvent {
+                code: Key::PageUp,
+                modifiers: KeyModifiers::NONE,
+            }) => self.perform(Cmd::Scroll(Direction::Up)),
+
+            Event::Keyboard(keyevent) if keyevent == keys.navigation_keys.goto_top.get() => {
+                self.perform(Cmd::GoTo(Position::Begin))
+            }
+            Event::Keyboard(keyevent) if keyevent == keys.navigation_keys.goto_bottom.get() => {
+                self.perform(Cmd::GoTo(Position::End))
+            }
+            Event::Keyboard(KeyEvent {
+                code: Key::Home,
+                modifiers: KeyModifiers::NONE,
+            }) => self.perform(Cmd::GoTo(Position::Begin)),
+            Event::Keyboard(KeyEvent {
+                code: Key::End,
+                modifiers: KeyModifiers::NONE,
+            }) => self.perform(Cmd::GoTo(Position::End)),
+
+            // file modifying
+            Event::Keyboard(keyevent) if keyevent == keys.library_keys.delete.get() => {
+                return Some(self.handle_delete());
+            }
+            Event::Keyboard(keyevent) if keyevent == keys.library_keys.yank.get() => {
+                self.yank();
+                CmdResult::None
+            }
+            Event::Keyboard(keyevent) if keyevent == keys.library_keys.paste.get() => {
+                match self.paste() {
+                    Ok(None) => CmdResult::None,
+                    Ok(Some(msg)) => return Some(Msg::Library(msg)),
+                    Err(err) => return Some(Msg::Library(LIMsg::PasteError(err.to_string()))),
+                }
+            }
+
+            // music root modification
+            Event::Keyboard(keyevent) if keyevent == keys.library_keys.cycle_root.get() => {
+                if let Some(path) = self.get_root_path() {
+                    return Some(Msg::Library(LIMsg::SwitchRoot(path.to_path_buf())));
+                }
+                CmdResult::None
+            }
+
+            Event::Keyboard(keyevent) if keyevent == keys.library_keys.add_root.get() => {
+                if let Some(path) = self.get_root_path() {
+                    return Some(Msg::Library(LIMsg::AddRoot(path.to_path_buf())));
+                }
+                CmdResult::None
+            }
+            Event::Keyboard(keyevent) if keyevent == keys.library_keys.remove_root.get() => {
+                if let Some(path) = self.get_root_path() {
+                    return Some(Msg::Library(LIMsg::RemoveRoot(path.to_path_buf())));
+                }
+                CmdResult::None
+            }
+
+            // load more tree
+            Event::Keyboard(KeyEvent {
+                code: Key::Backspace,
+                modifiers: KeyModifiers::NONE,
+            }) => {
+                let Some(current_root) = self.get_root_path() else {
+                    return None;
+                };
+                let parent = current_root.parent().unwrap_or(current_root);
+
+                // only trigger a load if we are not at the root of the filesystem already
+                if current_root != parent {
+                    self.trigger_load_with_focus(parent, Some(current_root.to_path_buf()));
+                }
+                // there is no special indicator or message; the download_tracker should force a draw once active
+                CmdResult::None
+            }
+            Event::Keyboard(KeyEvent {
+                code: Key::Enter,
+                modifiers: KeyModifiers::NONE,
+            }) => self.perform(Cmd::Submit),
+
+            // search
+            Event::Keyboard(keyevent) if keyevent == keys.library_keys.search.get() => {
+                if let Some(path) = self.get_root_path() {
+                    return Some(Msg::GeneralSearch(GSMsg::PopupShowLibrary(
+                        path.to_path_buf(),
+                    )));
+                }
+                CmdResult::None
+            }
+
+            Event::Keyboard(keyevent) if keyevent == keys.library_keys.youtube_search.get() => {
+                return Some(Msg::YoutubeSearch(YSMsg::InputPopupShow));
+            }
+
+            // load into playlist
+            Event::Keyboard(keyevent) if keyevent == keys.library_keys.load_dir.get() => {
+                if let Some(path) = self.get_selected_path()
+                    && path.is_dir()
+                {
+                    return Some(Msg::Playlist(PLMsg::Add(path.to_path_buf())));
+                }
+                CmdResult::None
+            }
+            Event::Keyboard(keyevent) if keyevent == keys.library_keys.load_track.get() => {
+                if let Some(path) = self.get_selected_path()
+                    && !path.is_dir()
+                {
+                    return Some(Msg::Playlist(PLMsg::Add(path.to_path_buf())));
+                }
+                CmdResult::None
+            }
+
+            // other
+            Event::Keyboard(
+                KeyEvent {
+                    code: Key::Tab,
+                    modifiers: KeyModifiers::NONE,
+                }
+                | KeyEvent {
+                    code: Key::BackTab,
+                    modifiers: KeyModifiers::SHIFT,
+                },
+            ) => return Some(Msg::Library(LIMsg::TreeBlur)),
+            Event::Keyboard(keyevent) if keyevent == keys.library_keys.open_tag_editor.get() => {
+                if let Some(path) = self.get_selected_path()
+                    && !path.is_dir()
+                {
+                    return Some(Msg::TagEditor(TEMsg::Open(path.to_path_buf())));
+                }
+                CmdResult::None
+            }
+
+            _ => CmdResult::None,
+        };
+        match result {
+            CmdResult::Submit(State::One(StateValue::String(node))) => {
+                let path = Path::new(&node);
+                if path.is_dir() {
+                    self.trigger_load_stepinto(path);
+                    // there is no special indicator or message; the download_tracker should force a draw once active
+                }
+                None
+            }
+            CmdResult::None => None,
+            _ => Some(Msg::ForceRedraw),
+        }
     }
 }
--- tui/src/ui/components/orx_music_library/scanner.rs
+++ tui/src/ui/components/orx_music_library/scanner.rs
@@ -1,1 +1,140 @@
 //! Functions to walk a given path and generate data for the component
+
+use std::path::{Path, PathBuf};
+
+use termusiclib::{config::v2::server::ScanDepth, utils::get_pin_yin};
+use tuirealm_orx_tree::types::{NodeIdx, Tree};
+
+use crate::ui::{
+    components::orx_music_library::music_library::MusicLibData,
+    model::{DownloadTracker, TxToMain},
+    msg::RecVec,
+};
+
+/// Execute a library scan on a different thread.
+///
+/// Executes [`library_dir_tree`] on a different thread and calls `cb` on finish.
+pub fn library_scan_cb<P: Into<PathBuf>, F>(
+    download_tracker: DownloadTracker,
+    path: P,
+    depth: ScanDepth,
+    cb: F,
+) where
+    F: FnOnce(RecVec) + Send + 'static,
+{
+    let path = path.into();
+    std::thread::Builder::new()
+        .name("library tree scan".to_string())
+        .spawn(move || {
+            download_tracker.increase_one(path.to_string_lossy());
+            let vec = library_dir_tree(&path, depth);
+
+            cb(vec);
+            download_tracker.decrease_one(&path.to_string_lossy());
+        })
+        .expect("Failed to spawn thread");
+}
+
+/// Execute a library scan on a different thread.
+///
+/// Executes [`library_dir_tree`] on a different thread and send a [`LIMsg::TreeNodeReady`] on finish
+pub fn library_scan<P: Into<PathBuf>>(
+    download_tracker: DownloadTracker,
+    path: P,
+    depth: ScanDepth,
+    tx: TxToMain,
+    focus_node: Option<NodeIdx<MusicLibData>>,
+) {
+    library_scan_cb(download_tracker, path, depth, move |vec| {
+        todo!();
+        // let _ = tx.send(Msg::Library(LIMsg::TreeNodeReady(LINodeReady {
+        //     vec,
+        //     focus_node,
+        // })));
+    });
+}
+
+/// Scan the given `path` for up to `depth`, and return a [`Node`] tree.
+///
+/// Note: consider using [`library_scan`] instead of this directly for running in a different thread.
+#[inline]
+pub fn library_dir_tree(path: &Path, depth: ScanDepth) -> RecVec {
+    library_dir_tree_inner(path, depth, None)
+}
+
+/// Scan the given `path` for up to `depth`, and return a [`Node`] tree.
+///
+/// Note: consider using [`library_scan`] instead of this directly for running in a different thread.
+fn library_dir_tree_inner(path: &Path, depth: ScanDepth, is_dir: Option<bool>) -> RecVec {
+    let is_dir = is_dir.unwrap_or_else(|| path.is_dir());
+    let mut node = RecVec {
+        path: path.to_path_buf(),
+        is_dir,
+        children: Vec::new(),
+    };
+
+    let depth = match depth {
+        ScanDepth::Limited(v) => v,
+        // put some kind of limit on it, thought the stack will likely overflow before this
+        ScanDepth::Unlimited => u32::MAX,
+    };
+
+    if depth > 0
+        && path.is_dir()
+        && let Ok(paths) = std::fs::read_dir(path)
+    {
+        let mut paths: Vec<(String, (PathBuf, bool))> = paths
+            .filter_map(std::result::Result::ok)
+            // filter out hidden files
+            .filter(|p| !p.file_name().to_string_lossy().starts_with('.'))
+            .map(|v| {
+                let sort_str = get_pin_yin(&v.file_name().to_string_lossy());
+                let is_dir = v.file_type().is_ok_and(|v| v.is_dir());
+                let path = v.path();
+                (sort_str, (path, is_dir))
+            })
+            .collect();
+
+        paths.sort_by(|a, b| alphanumeric_sort::compare_str(&a.0, &b.0));
+
+        for (_sort_str, (path, is_dir)) in paths {
+            node.children.push(library_dir_tree_inner(
+                &path,
+                ScanDepth::Limited(depth - 1),
+                Some(is_dir),
+            ));
+        }
+    }
+    node
+}
+
+/// Convert a [`RecVec`] to a [`Node`].
+pub fn recvec_to_tree(vec: RecVec) -> (NodeIdx<MusicLibData>, Tree<MusicLibData>) {
+    let mut tree = Tree::default();
+
+    (recvec_to_node_rec(vec, None, &mut tree), tree)
+}
+
+/// Convert the given `vec` to be child on `parent_node`.
+///
+/// If `parent_node` is `None` the new node will be pushed as the root.
+pub fn recvec_to_node_rec(
+    vec: RecVec,
+    parent_node: Option<&tuirealm_orx_tree::types::NodeIdx<MusicLibData>>,
+    tree: &mut tuirealm_orx_tree::types::Tree<MusicLibData>,
+) -> NodeIdx<MusicLibData> {
+    let is_dir = vec.path.is_dir();
+    let nodeidx = if let Some(idx) = parent_node {
+        tree.get_node_mut(idx)
+            .unwrap()
+            .push_child(MusicLibData::new(vec.path, is_dir))
+    } else {
+        tree.push_root(MusicLibData::new(vec.path, is_dir))
+    };
+
+    for val in vec.children {
+        recvec_to_node_rec(val, Some(&nodeidx), tree);
+    }
+
+    nodeidx
+}
