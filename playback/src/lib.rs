tramhao/termusic

commit d3d07d9c87647a1eade2e11e9b13380277a44b25

fix(playback::mpris/server): update media-controls reported volume on volume updates
instead of on-tick

server/src/server.rs
@@ -331,6 +331,7 @@ fn player_loop(
                 info!("after volumedown: {}", new_volume);
                 let mut p_tick = playerstats.lock();
                 p_tick.volume = new_volume;
+                player.mpris_volume_update();
             }
             PlayerCmd::VolumeUp => {
                 info!("before volumeup: {}", player.volume());
@@ -339,6 +340,7 @@ fn player_loop(
                 info!("after volumeup: {}", new_volume);
                 let mut p_tick = playerstats.lock();
                 p_tick.volume = new_volume;
+                player.mpris_volume_update();
             }
             PlayerCmd::Pause => {
                 player.pause();

playback/src/mpris.rs
@@ -227,11 +227,17 @@ impl GeneralPlayer {
             self.mpris_handler(m);
         }
 
-        self.mpris.update_volume(self.volume());
-
         if let Some(progress) = self.get_progress() {
             self.mpris
                 .update_progress(progress.position, self.playlist.status());
+        }
+    }
+
+    /// Update Media-Controls reported volume, if enabled to be reporting
+    #[inline]
+    pub fn mpris_volume_update(&mut self) {
+        if self.config.read().player_use_mpris {
+            self.mpris.update_volume(self.volume());
         }
     }
 }

commit c9a3fcb98686a746b4b9055d5f51454d40963284

fix(server / playback): update mpris volume on "PlayerTrait" volume set commands
Instead of only on specific events.

Likely broken since d3d07d9c87647a1eade2e11e9b13380277a44b25.

fixes #625

playback/src/lib.rs
@@ -627,12 +627,14 @@ impl PlayerTrait for GeneralPlayer {
     }
     fn add_volume(&mut self, volume: VolumeSigned) -> Volume {
         let vol = self.get_player_mut().add_volume(volume);
+        self.mpris_volume_update();
         self.send_stream_ev(UpdateEvents::VolumeChanged { volume: vol });
 
         vol
     }
     fn set_volume(&mut self, volume: Volume) -> Volume {
         let vol = self.get_player_mut().set_volume(volume);
+        self.mpris_volume_update();
         self.send_stream_ev(UpdateEvents::VolumeChanged { volume: vol });
 
         vol

server/src/server.rs
@@ -565,7 +565,6 @@ fn player_loop(
                 info!("after volumedown: {new_volume}");
                 let mut p_tick = playerstats.lock();
                 p_tick.volume = new_volume;
-                player.mpris_volume_update();
             }
             PlayerCmd::VolumeUp => {
                 info!("before volumeup: {}", player.volume());
@@ -574,7 +573,6 @@ fn player_loop(
                 info!("after volumeup: {new_volume}");
                 let mut p_tick = playerstats.lock();
                 p_tick.volume = new_volume;
-                player.mpris_volume_update();
             }
             PlayerCmd::Pause => {
                 player.pause();