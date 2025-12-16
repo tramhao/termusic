87674a03680d655a2df90667bd35a1d4aa6f5515
hasezoey <hasezoey@gmail.com>
2025-12-16 13:00:29 +0100

    refactor(lib::config::v1): remove unused functions

--- lib/src/config/v1/mod.rs
+++ lib/src/config/v1/mod.rs
@@ -1,8 +1,7 @@
 mod key;
 mod theme;
 
-use anyhow::{Context, Result, bail};
-use image::DynamicImage;
+use anyhow::{Context, Result};
 pub use key::{BindingForEvent, Keys};
 use serde::{Deserialize, Serialize};
 use std::{net::IpAddr, path::Path};
@@ -33,24 +32,6 @@ pub enum Loop {
     Random,
 }
 
-impl Loop {
-    pub fn display(self, display_symbol: bool) -> String {
-        if display_symbol {
-            match self {
-                Self::Single => "🔂".to_string(),
-                Self::Playlist => "🔁".to_string(),
-                Self::Random => "🔀".to_string(),
-            }
-        } else {
-            match self {
-                Self::Single => "single".to_string(),
-                Self::Playlist => "playlist".to_string(),
-                Self::Random => "random".to_string(),
-            }
-        }
-    }
-}
-
 #[derive(Clone, Deserialize, Serialize, Debug)]
 #[serde(default)] // allow missing fields and fill them with the `..Self::default()` in this struct
 pub struct Xywh {
@@ -76,36 +57,11 @@ pub enum Alignment {
     TopLeft,
 }
 
-impl Alignment {
-    const fn x(self, absolute_x: u32, width: u32) -> u32 {
-        match self {
-            Self::BottomRight | Self::TopRight => Self::get_size_substract(absolute_x, width),
-            Self::BottomLeft | Self::TopLeft => absolute_x,
-        }
-    }
-    const fn y(self, absolute_y: u32, height: u32) -> u32 {
-        match self {
-            Self::BottomRight | Self::BottomLeft => {
-                Self::get_size_substract(absolute_y, height / 2)
-            }
-            Self::TopRight | Self::TopLeft => absolute_y,
-        }
-    }
-
-    const fn get_size_substract(absolute_size: u32, size: u32) -> u32 {
-        if absolute_size > size {
-            return absolute_size - size;
-        }
-        0
-    }
-}
-
 impl Default for Xywh {
-    #[allow(clippy::cast_lossless, clippy::cast_possible_truncation)]
     fn default() -> Self {
         let width = 20_u32;
         let height = 20_u32;
-        let (term_width, term_height) = Self::get_terminal_size_u32();
+        let (term_width, term_height) = (50, 70);
         let x = term_width - 1;
         let y = term_height - 9;
 
@@ -119,89 +75,6 @@ impl Default for Xywh {
             height,
             align: Alignment::BottomRight,
         }
-    }
-}
-impl Xywh {
-    pub fn move_left(&mut self) {
-        self.x_between_1_100 = self.x_between_1_100.saturating_sub(1);
-    }
-
-    pub fn move_right(&mut self) {
-        self.x_between_1_100 += 1;
-        self.x_between_1_100 = self.x_between_1_100.min(100);
-    }
-
-    pub fn move_up(&mut self) {
-        self.y_between_1_100 = self.y_between_1_100.saturating_sub(2);
-    }
-
-    pub fn move_down(&mut self) {
-        self.y_between_1_100 += 2;
-        self.y_between_1_100 = self.y_between_1_100.min(100);
-    }
-    pub fn zoom_in(&mut self) {
-        self.width_between_1_100 += 1;
-        self.width_between_1_100 = self.width_between_1_100.min(100);
-    }
-
-    pub fn zoom_out(&mut self) {
-        self.width_between_1_100 = self.width_between_1_100.saturating_sub(1);
-    }
-
-    pub fn update_size(&self, image: &DynamicImage) -> Result<Self> {
-        let (term_width, term_height) = Self::get_terminal_size_u32();
-        let (x, y, width, height) = self.calculate_xywh(term_width, term_height, image)?;
-        Ok(Self {
-            x_between_1_100: self.x_between_1_100,
-            y_between_1_100: self.y_between_1_100,
-            width_between_1_100: self.width_between_1_100,
-            x,
-            y,
-            width,
-            height,
-            align: self.align,
-        })
-    }
-    fn calculate_xywh(
-        &self,
-        term_width: u32,
-        term_height: u32,
-        image: &DynamicImage,
-    ) -> Result<(u32, u32, u32, u32)> {
-        let width = self.get_width(term_width)?;
-        let height = Self::get_height(width, term_height, image)?;
-        let (absolute_x, absolute_y) = (
-            self.x_between_1_100 * term_width / 100,
-            self.y_between_1_100 * term_height / 100,
-        );
-        let (x, y) = (
-            self.align.x(absolute_x, width),
-            self.align.y(absolute_y, height),
-        );
-        Ok((x, y, width, height))
-    }
-
-    fn get_width(&self, term_width: u32) -> Result<u32> {
-        let width = self.width_between_1_100 * term_width / 100;
-        Self::safe_guard_width_or_height(width, term_width)
-    }
-
-    fn safe_guard_width_or_height(size: u32, size_max: u32) -> Result<u32> {
-        if size > size_max {
-            bail!("image width is too big, please reduce image width");
-        }
-        Ok(size)
-    }
-
-    fn get_height(width: u32, term_height: u32, image: &DynamicImage) -> Result<u32> {
-        let (pic_width_orig, pic_height_orig) = image::GenericImageView::dimensions(image);
-        let height = (width * pic_height_orig) / (pic_width_orig);
-        Self::safe_guard_width_or_height(height, term_height * 2)
-    }
-
-    pub fn get_terminal_size_u32() -> (u32, u32) {
-        let (term_width, term_height) = viuer::terminal_size();
-        (u32::from(term_width), u32::from(term_height))
     }
 }
 
