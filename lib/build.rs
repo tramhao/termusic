aa1524def63bb1ec24e0fe3f6d4ea4bd0c74ac00
hasezoey <hasezoey@gmail.com>
2026-04-04 15:19:37 +0200

    refactor(lib): remove unnecessary MonitorService again

--- lib/build.rs
+++ lib/build.rs
@@ -1,5 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::compile_protos("proto/player.proto")?;
-    tonic_prost_build::compile_protos("proto/monitor.proto")?;
     Ok(())
 }
