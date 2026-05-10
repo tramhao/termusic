4f2620bbe5b374e9e55a5a810d17721048836264
hasezoey <hasezoey@gmail.com>
2026-05-10 18:17:18 +0200

    fix(tui::ui::model::playlist): update "current track index" on swap

--- tui/src/ui/model/playlist.rs
+++ tui/src/ui/model/playlist.rs
@@ -57,6 +57,15 @@ impl TUIPlaylist {
             bail!("Index {} not within tracks bounds", index_a.max(index_b));
         }
 
+        // maintain the correct current track index after swap
+        if let Some(current_track_idx) = self.current_track_idx {
+            if current_track_idx == index_a {
+                self.current_track_idx = Some(index_b);
+            } else if current_track_idx == index_b {
+                self.current_track_idx = Some(index_a);
+            }
+        }
+
         self.tracks.swap(index_a, index_b);
 
         Ok(())
