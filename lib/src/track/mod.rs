95b26795bb113b52bd3b3a7eb177562b5790769b
hasezoey <hasezoey@gmail.com>
2026-07-27 12:13:15 +0200

    refactor(lib::track): move metadata fetching to own module

--- lib/src/track/mod.rs
+++ lib/src/track/mod.rs
@@ -1,8 +1,9 @@
 mod fmt_ext;
 mod full_track;
+mod read_metadata;
 
 pub use fmt_ext::DurationFmtShort;
 pub use full_track::{
-    LyricData, MediaTypes, MediaTypesSimple, MetadataOptions, PodcastTrackData, RadioTrackData,
-    Track, TrackData, TrackMetadata, parse_metadata_from_file,
+    LyricData, MediaTypes, MediaTypesSimple, PodcastTrackData, RadioTrackData, Track, TrackData,
 };
+pub use read_metadata::{MetadataOptions, TrackMetadata, parse_metadata_from_file};
